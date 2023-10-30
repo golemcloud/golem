f0caa0f2d7cd8e89cb4ad6c4e63eafad61c89d7d
Nico Burniske <nicoburniske@gmail.com>
2023-10-30 12:49:23 -0400

    initial setup nextra docs site

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
-module.exports = nextConfig
+module.exports = withNextra({
+  reactStrictMode: true,
+});
