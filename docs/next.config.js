245daef1e26139e784b82db14a33e31c5632354b
Nico Burniske <nicoburniske@gmail.com>
2023-10-31 09:16:50 -0400

    add prettier and format

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,11 +1,11 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-});
+})
 
 module.exports = withNextra({
   reactStrictMode: true,
   experimental: {
     scrollRestoration: true,
   },
-});
+})
---
+++ docs/prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [],
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  printWidth: 100,
+  arrowParens: "avoid",
+  jsxSingleQuote: false,
+}
