df0406a52c4545f965dee8872072257044baae70
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2023-12-03 21:38:06 +0000

    Fix

--- golem-common/build.rs
+++ golem-common/build.rs
@@ -13,21 +13,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .include_file("mod.rs")
         .compile(
             &[
-                "../proto/services/account_service.proto",
-                "../proto/services/account_summary_service.proto",
-                "../proto/services/grant_service.proto",
-                "../proto/services/limits_service.proto",
-                "../proto/services/login_service.proto",
-                "../proto/services/project_grant_service.proto",
-                "../proto/services/project_policy_service.proto",
-                "../proto/services/project_service.proto",
-                "../proto/services/shard_manager_service.proto",
-                "../proto/services/template_service.proto",
-                "../proto/services/token_service.proto",
-                "../proto/services/worker_executor.proto",
-                "../proto/services/worker_service.proto",
+                "proto/services/account_service.proto",
+                "proto/services/account_summary_service.proto",
+                "proto/services/grant_service.proto",
+                "proto/services/limits_service.proto",
+                "proto/services/login_service.proto",
+                "proto/services/project_grant_service.proto",
+                "proto/services/project_policy_service.proto",
+                "proto/services/project_service.proto",
+                "proto/services/shard_manager_service.proto",
+                "proto/services/template_service.proto",
+                "proto/services/token_service.proto",
+                "proto/services/worker_executor.proto",
+                "proto/services/worker_service.proto",
             ],
-            &["../proto"],
+            &["proto"],
         )
         .unwrap();
 
