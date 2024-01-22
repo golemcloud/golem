0a1c897fcaa250262d267d48cbdace74e4987e0c
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-01-22 12:48:43 +0100

    Simulate test failure to see how it looks like

--- golem-worker-executor-base/tests/wasi.rs
+++ golem-worker-executor-base/tests/wasi.rs
@@ -29,7 +29,7 @@ async fn write_stdout() {
 
     drop(executor);
 
-    check!(events == vec![common::stdout_event("Sample text written to the output\n")]);
+    check!(events == vec![common::stdout_event("Sample text written to the output NOT GOOD\n")]);
 }
 
 #[tokio::test]
