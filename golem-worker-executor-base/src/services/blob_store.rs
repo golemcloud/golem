0f21fcc6e90028287920ab4a4533268a0c7e400e
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-01-23 14:15:52 +0100

    Local file system implementation of blob store

--- golem-worker-executor-base/src/services/blob_store.rs
+++ golem-worker-executor-base/src/services/blob_store.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::path::{Path, PathBuf};
 use std::sync::{Arc, RwLock};
 
 use async_trait::async_trait;
@@ -9,6 +10,7 @@ use golem_common::model::AccountId;
 use s3::config::Region;
 use serde::{Deserialize, Serialize};
 use tokio::io::AsyncReadExt;
+use tokio_stream::StreamExt;
 
 use crate::services::golem_config::{BlobStoreServiceConfig, BlobStoreServiceS3Config};
 
@@ -127,6 +129,11 @@ pub async fn configured(
             })
         }
         BlobStoreServiceConfig::InMemory(_) => Arc::new(BlobStoreServiceInMemory::new()),
+        BlobStoreServiceConfig::Local(config) => Arc::new(
+            BlobStoreServiceLocal::new(&config.root)
+                .await
+                .expect("Failed to create local blob store"),
+        ),
     }
 }
 
@@ -718,10 +725,498 @@ impl BlobStoreService for BlobStoreServiceInMemory {
     }
 }
 
+pub struct BlobStoreServiceLocal {
+    root: PathBuf,
+}
+
+impl BlobStoreServiceLocal {
+    pub async fn new(root: &Path) -> anyhow::Result<Self> {
+        let canonical = async_fs::canonicalize(root).await?;
+        Ok(Self { root: canonical })
+    }
+
+    fn container_path(
+        &self,
+        account_id: &AccountId,
+        container_name: &String,
+    ) -> anyhow::Result<PathBuf> {
+        let path = self.root.join(account_id.to_string()).join(container_name);
+        if path.starts_with(&self.root) {
+            Ok(path)
+        } else {
+            anyhow::bail!("Invalid container path pointing outside of the root directory for {account_id}/{container_name}");
+        }
+    }
+
+    fn object_path(
+        &self,
+        account_id: &AccountId,
+        container_name: &String,
+        object_name: &String,
+    ) -> anyhow::Result<PathBuf> {
+        let path = self
+            .container_path(account_id, container_name)?
+            .join(object_name);
+        if path.starts_with(&self.root) {
+            Ok(path)
+        } else {
+            anyhow::bail!("Invalid object path pointing outside of the root directory for {account_id}/{container_name}");
+        }
+    }
+}
+
+#[async_trait]
+impl BlobStoreService for BlobStoreServiceLocal {
+    async fn clear(&self, account_id: AccountId, container_name: String) -> anyhow::Result<()> {
+        let container_path = self.container_path(&account_id, &container_name)?;
+        if async_fs::metadata(&container_path).await.is_ok() {
+            async_fs::remove_dir_all(&container_path).await?;
+            async_fs::create_dir_all(&container_path).await?;
+        }
+        Ok(())
+    }
+
+    async fn container_exists(
+        &self,
+        account_id: AccountId,
+        container_name: String,
+    ) -> anyhow::Result<bool> {
+        let container_path = self.container_path(&account_id, &container_name)?;
+        Ok(async_fs::metadata(&container_path).await.is_ok())
+    }
+
+    async fn copy_object(
+        &self,
+        account_id: AccountId,
+        source_container_name: String,
+        source_object_name: String,
+        destination_container_name: String,
+        destination_object_name: String,
+    ) -> anyhow::Result<()> {
+        let source_path =
+            self.object_path(&account_id, &source_container_name, &source_object_name)?;
+        let destination_path = self.object_path(
+            &account_id,
+            &destination_container_name,
+            &destination_object_name,
+        )?;
+
+        async_fs::copy(&source_path, &destination_path).await?;
+        Ok(())
+    }
+
+    async fn create_container(
+        &self,
+        account_id: AccountId,
+        container_name: String,
+    ) -> anyhow::Result<u64> {
+        let container_path = self.container_path(&account_id, &container_name)?;
+        async_fs::create_dir_all(&container_path).await?;
+        let metadata = async_fs::metadata(&container_path).await?;
+        let created_at = metadata
+            .created()?
+            .duration_since(std::time::UNIX_EPOCH)?
+            .as_millis()
+            .try_into()?;
+        Ok(created_at)
+    }
+
+    async fn delete_container(
+        &self,
+        account_id: AccountId,
+        container_name: String,
+    ) -> anyhow::Result<()> {
+        let container_path = self.container_path(&account_id, &container_name)?;
+        async_fs::remove_dir_all(&container_path).await?;
+        Ok(())
+    }
+
+    async fn delete_object(
+        &self,
+        account_id: AccountId,
+        container_name: String,
+        object_name: String,
+    ) -> anyhow::Result<()> {
+        let object_path = self.object_path(&account_id, &container_name, &object_name)?;
+        async_fs::remove_file(&object_path).await?;
+        Ok(())
+    }
+
+    async fn delete_objects(
+        &self,
+        account_id: AccountId,
+        container_name: String,
+        object_names: Vec<String>,
+    ) -> anyhow::Result<()> {
+        for object_name in object_names {
+            self.delete_object(account_id.clone(), container_name.clone(), object_name)
+                .await?;
+        }
+        Ok(())
+    }
+
+    async fn get_container(
+        &self,
+        account_id: AccountId,
+        container_name: String,
+    ) -> anyhow::Result<Option<u64>> {
+        let container_path = self.container_path(&account_id, &container_name)?;
+        match async_fs::metadata(&container_path).await {
+            Ok(metadata) => {
+                let created_at = metadata
+                    .created()?
+                    .duration_since(std::time::UNIX_EPOCH)?
+                    .as_millis()
+                    .try_into()?;
+                Ok(Some(created_at))
+            }
+            Err(_) => Ok(None),
+        }
+    }
+
+    async fn get_data(
+        &self,
+        account_id: AccountId,
+        container_name: String,
+        object_name: String,
+        start: u64,
+        end: u64,
+    ) -> anyhow::Result<Vec<u8>> {
+        let object_path = self.object_path(&account_id, &container_name, &object_name)?;
+        let data = async_fs::read(&object_path).await?;
+        Ok(data[start as usize..end as usize].to_vec())
+    }
+
+    async fn has_object(
+        &self,
+        account_id: AccountId,
+        container_name: String,
+        object_name: String,
+    ) -> anyhow::Result<bool> {
+        let object_path = self.object_path(&account_id, &container_name, &object_name)?;
+        Ok(async_fs::metadata(&object_path).await.is_ok())
+    }
+
+    async fn list_objects(
+        &self,
+        account_id: AccountId,
+        container_name: String,
+    ) -> anyhow::Result<Vec<String>> {
+        let container_path = self.container_path(&account_id, &container_name)?;
+        let mut object_names = Vec::new();
+        let mut dir = async_fs::read_dir(&container_path).await?;
+        while let Some(entry) = dir.try_next().await? {
+            object_names.push(entry.file_name().to_string_lossy().to_string());
+        }
+        Ok(object_names)
+    }
+
+    async fn move_object(
+        &self,
+        account_id: AccountId,
+        source_container_name: String,
+        source_object_name: String,
+        destination_container_name: String,
+        destination_object_name: String,
+    ) -> anyhow::Result<()> {
+        let source_path =
+            self.object_path(&account_id, &source_container_name, &source_object_name)?;
+        let destination_path = self.object_path(
+            &account_id,
+            &destination_container_name,
+            &destination_object_name,
+        )?;
+
+        async_fs::rename(&source_path, &destination_path).await?;
+        Ok(())
+    }
+
+    async fn object_info(
+        &self,
+        account_id: AccountId,
+        container_name: String,
+        object_name: String,
+    ) -> anyhow::Result<ObjectMetadata> {
+        let object_path = self.object_path(&account_id, &container_name, &object_name)?;
+        let metadata = async_fs::metadata(&object_path).await?;
+        let created_at = metadata
+            .created()?
+            .duration_since(std::time::UNIX_EPOCH)?
+            .as_millis()
+            .try_into()?;
+        let size = metadata.len();
+        Ok(ObjectMetadata {
+            name: object_name,
+            container: container_name,
+            created_at,
+            size,
+        })
+    }
+
+    async fn write_data(
+        &self,
+        account_id: AccountId,
+        container_name: String,
+        object_name: String,
+        data: Vec<u8>,
+    ) -> anyhow::Result<()> {
+        let object_path = self.object_path(&account_id, &container_name, &object_name)?;
+        async_fs::write(&object_path, data).await?;
+        Ok(())
+    }
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize, Encode, Decode)]
 pub struct ObjectMetadata {
     pub name: String,
     pub container: String,
     pub created_at: u64,
     pub size: u64,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::services::blob_store::BlobStoreService;
+    use golem_common::model::AccountId;
+    use tempfile::TempDir;
+
+    async fn test_container_exists(blob_store: &impl BlobStoreService) {
+        let account1 = AccountId {
+            value: "account1".to_string(),
+        };
+        assert_eq!(
+            blob_store
+                .container_exists(account1.clone(), "container1".to_string())
+                .await
+                .unwrap(),
+            false
+        );
+        blob_store
+            .create_container(account1.clone(), "container1".to_string())
+            .await
+            .unwrap();
+        assert_eq!(
+            blob_store
+                .container_exists(account1.clone(), "container1".to_string())
+                .await
+                .unwrap(),
+            true
+        );
+    }
+
+    async fn test_container_delete(blob_store: &impl BlobStoreService) {
+        let account1 = AccountId {
+            value: "account1".to_string(),
+        };
+        blob_store
+            .create_container(account1.clone(), "container1".to_string())
+            .await
+            .unwrap();
+        blob_store
+            .delete_container(account1.clone(), "container1".to_string())
+            .await
+            .unwrap();
+        assert_eq!(
+            blob_store
+                .container_exists(account1.clone(), "container1".to_string())
+                .await
+                .unwrap(),
+            false
+        );
+    }
+
+    async fn test_container_has_write_read_has(blob_store: &impl BlobStoreService) {
+        let account1 = AccountId {
+            value: "account1".to_string(),
+        };
+
+        blob_store
+            .create_container(account1.clone(), "container1".to_string())
+            .await
+            .unwrap();
+        assert_eq!(
+            blob_store
+                .has_object(
+                    account1.clone(),
+                    "container1".to_string(),
+                    "obj1".to_string()
+                )
+                .await
+                .unwrap(),
+            false
+        );
+
+        let original_data = vec![1, 2, 3, 4];
+        blob_store
+            .write_data(
+                account1.clone(),
+                "container1".to_string(),
+                "obj1".to_string(),
+                original_data.clone(),
+            )
+            .await
+            .unwrap();
+
+        let read_data = blob_store
+            .get_data(
+                account1.clone(),
+                "container1".to_string(),
+                "obj1".to_string(),
+                0,
+                4,
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(original_data, read_data);
+        assert_eq!(
+            blob_store
+                .has_object(
+                    account1.clone(),
+                    "container1".to_string(),
+                    "obj1".to_string()
+                )
+                .await
+                .unwrap(),
+            true
+        );
+    }
+
+    async fn test_container_list_copy_move_list(blob_store: &impl BlobStoreService) {
+        let account1 = AccountId {
+            value: "account1".to_string(),
+        };
+
+        blob_store
+            .create_container(account1.clone(), "container1".to_string())
+            .await
+            .unwrap();
+        blob_store
+            .create_container(account1.clone(), "container2".to_string())
+            .await
+            .unwrap();
+
+        assert!(blob_store
+            .list_objects(account1.clone(), "container1".to_string(),)
+            .await
+            .unwrap()
+            .is_empty());
+
+        let original_data = vec![1, 2, 3, 4];
+        blob_store
+            .write_data(
+                account1.clone(),
+                "container1".to_string(),
+                "obj1".to_string(),
+                original_data.clone(),
+            )
+            .await
+            .unwrap();
+
+        blob_store
+            .copy_object(
+                account1.clone(),
+                "container1".to_string(),
+                "obj1".to_string(),
+                "container1".to_string(),
+                "obj2".to_string(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(
+            blob_store
+                .list_objects(account1.clone(), "container1".to_string(),)
+                .await
+                .unwrap(),
+            vec!["obj1", "obj2"]
+        );
+
+        blob_store
+            .move_object(
+                account1.clone(),
+                "container1".to_string(),
+                "obj1".to_string(),
+                "container2".to_string(),
+                "obj3".to_string(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(
+            blob_store
+                .list_objects(account1.clone(), "container1".to_string(),)
+                .await
+                .unwrap(),
+            vec!["obj2"]
+        );
+
+        assert_eq!(
+            blob_store
+                .list_objects(account1.clone(), "container2".to_string(),)
+                .await
+                .unwrap(),
+            vec!["obj3"]
+        );
+    }
+
+    #[tokio::test]
+    async fn test_container_exists_in_memory() {
+        let blob_store = super::BlobStoreServiceInMemory::new();
+        test_container_exists(&blob_store).await;
+    }
+
+    #[tokio::test]
+    async fn test_container_exists_local() {
+        let tempdir = TempDir::new().unwrap();
+        let blob_store = super::BlobStoreServiceLocal::new(tempdir.path())
+            .await
+            .unwrap();
+        test_container_exists(&blob_store).await;
+    }
+
+    #[tokio::test]
+    async fn test_container_delete_in_memory() {
+        let blob_store = super::BlobStoreServiceInMemory::new();
+        test_container_delete(&blob_store).await;
+    }
+
+    #[tokio::test]
+    async fn test_container_delete_local() {
+        let tempdir = TempDir::new().unwrap();
+        let blob_store = super::BlobStoreServiceLocal::new(tempdir.path())
+            .await
+            .unwrap();
+        test_container_delete(&blob_store).await;
+    }
+
+    #[tokio::test]
+    async fn test_container_has_write_read_has_in_memory() {
+        let blob_store = super::BlobStoreServiceInMemory::new();
+        test_container_has_write_read_has(&blob_store).await;
+    }
+
+    #[tokio::test]
+    async fn test_container_has_write_read_has_local() {
+        let tempdir = TempDir::new().unwrap();
+        let blob_store = super::BlobStoreServiceLocal::new(tempdir.path())
+            .await
+            .unwrap();
+        test_container_has_write_read_has(&blob_store).await;
+    }
+
+    #[tokio::test]
+    async fn test_container_list_copy_move_list_in_memory() {
+        let blob_store = super::BlobStoreServiceInMemory::new();
+        test_container_list_copy_move_list(&blob_store).await;
+    }
+
+    #[tokio::test]
+    async fn test_container_list_copy_move_list_local() {
+        let tempdir = TempDir::new().unwrap();
+        let blob_store = super::BlobStoreServiceLocal::new(tempdir.path())
+            .await
+            .unwrap();
+        test_container_list_copy_move_list(&blob_store).await;
+    }
 }
