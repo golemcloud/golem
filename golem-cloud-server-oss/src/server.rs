77c4cb96dea15dd88824996d62013feb063b8701
Peter Kotula <peto.kotula@yahoo.com>
2024-01-29 18:01:21 +0100

    cloud-server-oss logging

--- golem-cloud-server-oss/src/server.rs
+++ golem-cloud-server-oss/src/server.rs
@@ -5,10 +5,29 @@ use cloud_server_oss::{api, grpcapi};
 use poem::listener::TcpListener;
 use std::net::{Ipv4Addr, SocketAddrV4};
 use tokio::select;
-use tracing::{debug, error};
+use tracing::{error, info};
+use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::EnvFilter;
 
 fn main() -> Result<(), std::io::Error> {
     let config = CloudServiceConfig::new();
+
+    if config.enable_tracing_console {
+        // NOTE: also requires RUSTFLAGS="--cfg tokio_unstable" cargo build
+        console_subscriber::init();
+    } else if config.enable_json_log {
+        tracing_subscriber::fmt()
+            .json()
+            .flatten_event(true)
+            .with_span_events(FmtSpan::FULL) // NOTE: enable to see span events
+            .with_env_filter(EnvFilter::from_default_env())
+            .init();
+    } else {
+        tracing_subscriber::fmt()
+            .with_env_filter(EnvFilter::from_default_env())
+            .with_ansi(true)
+            .init();
+    }
 
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
@@ -21,7 +40,7 @@ async fn async_main(config: &CloudServiceConfig) -> Result<(), std::io::Error> {
     let grpc_port = config.grpc_port;
     let http_port = config.http_port;
 
-    debug!(
+    info!(
         "Starting cloud server on ports: http: {}, grpc: {}",
         http_port, grpc_port
     );
