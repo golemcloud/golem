9770cdeb0c921a5978564d7c6e329434aeb31ffb
Peter Kotula <peto.kotula@yahoo.com>
2024-01-31 08:56:25 +0100

    modules renaming (#71)
    * cloud-server-oss renamed to golem-service, golem-worker-executor-oss renamed to golem-worker-executor

--- golem-cloud-server-oss/src/openapi_yaml.rs
+++
@@ -1,7 +0,0 @@
-use cloud_server_oss::api::make_open_api_service;
-use cloud_server_oss::service::Services;
-
-fn main() {
-    let service = make_open_api_service(&Services::noop());
-    println!("{}", service.spec_yaml())
-}
