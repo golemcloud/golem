766852381a6d41046510fed4f3a5c5bbce011999
Afsal Thaj <afsal.taj06@gmail.com>
2024-02-09 00:50:38 +1100

    Fix docker compose

--- golem-worker-bridge/src/app_config.rs
+++ golem-worker-bridge/src/app_config.rs
@@ -67,8 +67,8 @@ mod tests {
         std::env::set_var("GOLEM__REDIS__DATABASE", "1");
         std::env::set_var("GOLEM__ENVIRONMENT", "dev");
         std::env::set_var("GOLEM__WORKSPACE", "release");
-        std::env::set_var("GOLEM__COMPONENT_SERVICE__HOST", "localhost");
-        std::env::set_var("GOLEM__COMPONENT_SERVICE__PORT", "1234");
+        std::env::set_var("GOLEM__TEMPLATE_SERVICE__HOST", "localhost");
+        std::env::set_var("GOLEM__TEMPLATE_SERVICE__PORT", "1234");
         std::env::set_var(
             "GOLEM__TEMPLATE_SERVICE__ACCESS_TOKEN",
             "5C832D93-FF85-4A8F-9803-513950FDFDB1",
