b0bc58162cb1bf5a21218b5b320f81a467122bb8
Peter Kotula <peto.kotula@yahoo.com>
2024-02-09 21:54:27 +0100

    golem-service - prometheus metrics (#108)

--- golem-service/src/metrics.rs
+++ golem-service/src/metrics.rs
@@ -12,10 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod api;
-pub mod config;
-pub mod db;
-pub mod grpcapi;
-pub mod metrics;
-pub mod repo;
-pub mod service;
+use lazy_static::lazy_static;
+use prometheus::*;
+
+lazy_static! {
+    static ref VERSION_INFO: IntCounterVec =
+        register_int_counter_vec!("version_info", "Version info of the server", &["version"])
+            .unwrap();
+}
+
+pub fn register_all() -> Registry {
+    VERSION_INFO.with_label_values(&[version()]).inc();
+
+    default_registry().clone()
+}
+
+fn version() -> &'static str {
+    option_env!("CARGO_VERSION_INFO").unwrap_or(env!("CARGO_PKG_VERSION"))
+}
