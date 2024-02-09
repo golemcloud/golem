49c37641802daee4647c31753a53a1efbb3926a2
Adam Fraser <adam.fraser@gmail.com>
2024-02-08 16:12:03 -0800

    Add License (#107)
    * add license
    
    * cleanup
    
    * cleanup

--- golem-common/src/model.rs
+++ golem-common/src/model.rs
@@ -1,3 +1,17 @@
+// Copyright 2024 Golem Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use std::borrow::Cow;
 use std::collections::HashSet;
 use std::fmt::{Display, Formatter};
@@ -325,7 +339,7 @@ impl ShardId {
         ((high as i64) << 32) | ((low as i64) & 0xFFFFFFFF)
     }
 
-    fn hash_string(string: &String) -> i32 {
+    fn hash_string(string: &str) -> i32 {
         let mut hash = 0;
         if hash == 0 && !string.is_empty() {
             for val in &mut string.bytes() {
