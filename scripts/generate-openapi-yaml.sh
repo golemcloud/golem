b5b84c1eb1394955740b92a1d863150fd2a88bd6
Nico Burniske <nicoburniske@gmail.com>
2024-02-21 11:58:19 -0500

    attempt fix openapi yaml generation

--- scripts/generate-openapi-yaml.sh
+++ scripts/generate-openapi-yaml.sh
@@ -3,7 +3,14 @@ set -uo pipefail
 
 script_full_path=$(dirname "$0")
 
-cd "${script_full_path}"/../golem-service || exit
+# Template service
 
+cd "${script_full_path}"/../golem-template-service || exit
 cargo build
-../target/debug/golem-service-yaml > ../openapi/golem-service.yaml
+../target/debug/golem-template-service-yaml > ../openapi/golem-template-service.yaml
+
+# Worker service
+
+cd "${script_full_path}"/../golem-worker-service || exit
+cargo build
+../target/debug/golem-worker-service-yaml > ../openapi/golem-worker-service.yaml
