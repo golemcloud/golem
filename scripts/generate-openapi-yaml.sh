59bdd5c1bc90bbf24e71293216ee5c1c661f4bdf
Nico Burniske <nicoburniske@gmail.com>
2024-02-27 12:54:46 -0500

    fix openapi difference + fix scripts + fix yaml

--- scripts/generate-openapi-yaml.sh
+++ scripts/generate-openapi-yaml.sh
@@ -3,6 +3,22 @@ set -uo pipefail
 
 script_full_path=$(dirname "$0")
 
-cd "${script_full_path}"/../golem-public-api || exit
+# Template service
+pushd "${script_full_path}"/../golem-template-service || exit
+cargo build
+../target/debug/golem-template-service-yaml > ../openapi/golem-template-service.yaml
 
-../target/debug/golem-service-yaml > ../openapi/golem-service.yaml
+# Pop back to the original directory
+popd || exit
+
+pushd "${script_full_path}"/../golem-worker-service || exit
+cargo build
+../target/debug/golem-worker-service-yaml > ../openapi/golem-worker-service.yaml
+
+# Pop back to the original directory when done
+popd || exit
+
+# Merge API specs
+pushd "${script_full_path}"/../golem-openapi-client-generator || exit
+cargo build
+cargo run -- merge --spec-yaml ../openapi/golem-template-service.yaml ../openapi/golem-worker-service.yaml --output-yaml ../openapi/golem-service.yaml
