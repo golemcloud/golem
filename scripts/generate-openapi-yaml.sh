bf41dafceb0f6bc0f16b603d36b90aa7f126c85d
Nico Burniske <nicoburniske@gmail.com>
2024-02-27 12:57:51 -0500

    pop at end of script

--- scripts/generate-openapi-yaml.sh
+++ scripts/generate-openapi-yaml.sh
@@ -8,17 +8,17 @@ pushd "${script_full_path}"/../golem-template-service || exit
 cargo build
 ../target/debug/golem-template-service-yaml > ../openapi/golem-template-service.yaml
 
-# Pop back to the original directory
 popd || exit
 
 pushd "${script_full_path}"/../golem-worker-service || exit
 cargo build
 ../target/debug/golem-worker-service-yaml > ../openapi/golem-worker-service.yaml
 
-# Pop back to the original directory when done
 popd || exit
 
 # Merge API specs
 pushd "${script_full_path}"/../golem-openapi-client-generator || exit
 cargo build
 cargo run -- merge --spec-yaml ../openapi/golem-template-service.yaml ../openapi/golem-worker-service.yaml --output-yaml ../openapi/golem-service.yaml
+
+popd || exit
