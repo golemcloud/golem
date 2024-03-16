ade3c74a4470358d94accc0b085384e1b2affbd6
Nico Burniske <46934294+nicoburniske@users.noreply.github.com>
2024-03-16 13:21:45 -0400

    GOL-188 Compilation Server (#150)
    * initial compilation server
    
    * progress
    
    * remove cancellation token, fix config, and integrate template_id instead of UUID in grpc
    
    * read grpc host from config
    
    * rename compilation service to template compilation service
    
    * integrate into golem-service
    
    * add metrics
    
    * add cargo lock
    
    * port changes
    
    * remove duplicate entry from grpc build
    
    * fix unit tests for config
    
    * remove cargo config
    
    * remove golem-service dir
    
    * re-add dependency to cargo toml
    
    * rename template-compilation to template-compilation-service
    
    * update dockerfile to use 8080
    
    * fix docker compose for local dev and add logs
    
    * add template compilation service to ci

---
+++ golem-template-compilation-service/src/metrics.rs
@@ -0,0 +1,22 @@
+use lazy_static::lazy_static;
+use prometheus::*;
+
+lazy_static! {
+    static ref COMPILATION_QUEUE_LENGTH: Gauge = register_gauge!(
+        "template_compilation_queue_length",
+        "Number of outstanding compilation requests"
+    )
+    .unwrap();
+}
+
+pub fn increment_queue_length() {
+    COMPILATION_QUEUE_LENGTH.inc();
+}
+
+pub fn decrement_queue_length() {
+    COMPILATION_QUEUE_LENGTH.dec();
+}
+
+pub fn register_all() -> Registry {
+    default_registry().clone()
+}
