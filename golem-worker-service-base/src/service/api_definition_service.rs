28273d3c06727a5cf15fa6208e2620c41bef80d3
Afsal Thaj <afsal.taj06@gmail.com>
2024-03-18 19:40:12 +0100

    Initial commit on returning namespace

--- golem-worker-service-base/src/service/api_definition_service.rs
+++ golem-worker-service-base/src/service/api_definition_service.rs
@@ -16,29 +16,35 @@ pub trait ApiDefinitionService<Namespace, AuthCtx> {
         &self,
         definition: &ApiDefinition,
         auth_ctx: AuthCtx,
-    ) -> Result<(), ApiRegistrationError>;
+    ) -> Result<ApiDefinitionIdAnnotated<Namespace>, ApiRegistrationError>;
 
     async fn get(
         &self,
         api_definition_id: &ApiDefinitionId,
         version: &Version,
         auth_ctx: AuthCtx,
-    ) -> Result<Option<ApiDefinition>, ApiRegistrationError>;
+    ) -> Result<Option<ApiDefinitionIdAnnotated<Namespace>>, ApiRegistrationError>;
 
     async fn delete(
         &self,
         api_definition_id: &ApiDefinitionId,
         version: &Version,
         auth_ctx: AuthCtx,
-    ) -> Result<bool, ApiRegistrationError>;
+    ) -> Result<ApiDefinitionIdAnnotated<Namespace>, ApiRegistrationError>;
 
-    async fn get_all(&self, auth_ctx: AuthCtx) -> Result<Vec<ApiDefinition>, ApiRegistrationError>;
+    async fn get_all(&self, auth_ctx: AuthCtx) -> Result<Vec<ApiDefinitionIdAnnotated<Namespace>>, ApiRegistrationError>;
 
     async fn get_all_versions(
         &self,
         api_id: &ApiDefinitionId,
         auth_ctx: AuthCtx,
-    ) -> Result<Vec<ApiDefinition>, ApiRegistrationError>;
+    ) -> Result<Vec<ApiDefinitionIdAnnotated<Namespace>>, ApiRegistrationError>;
+}
+
+pub struct ApiDefinitionIdAnnotated<Namespace> {
+    pub namespace: Namespace,
+    pub api_definition: ApiDefinitionId,
+
 }
 
 // An ApiDefinitionKey is just the original ApiDefinitionId with additional information of version and a possibility of namespace.
@@ -243,10 +249,13 @@ pub struct RegisterApiDefinitionNoop {}
 impl ApiDefinitionService<CommonNamespace, EmptyAuthCtx> for RegisterApiDefinitionNoop {
     async fn register(
         &self,
-        _definition: &ApiDefinition,
+        definition: &ApiDefinition,
         _auth_ctx: EmptyAuthCtx,
-    ) -> Result<(), ApiRegistrationError> {
-        Ok(())
+    ) -> Result<ApiDefinitionIdAnnotated<CommonNamespace>, ApiRegistrationError> {
+        Ok(ApiDefinitionIdAnnotated {
+            namespace: CommonNamespace::default(),
+            api_definition: definition.id.clone(),
+        })
     }
 
     async fn get(
@@ -254,17 +263,20 @@ impl ApiDefinitionService<CommonNamespace, EmptyAuthCtx> for RegisterApiDefiniti
         _api_definition_id: &ApiDefinitionId,
         _version: &Version,
         _auth_ctx: EmptyAuthCtx,
-    ) -> Result<Option<ApiDefinition>, ApiRegistrationError> {
+    ) -> Result<Option<ApiDefinitionIdAnnotated<CommonNamespace>>, ApiRegistrationError> {
         Ok(None)
     }
 
     async fn delete(
         &self,
-        _api_definition_id: &ApiDefinitionId,
+        api_definition_id: &ApiDefinitionId,
         _version: &Version,
         _auth_ctx: EmptyAuthCtx,
-    ) -> Result<bool, ApiRegistrationError> {
-        Ok(false)
+    ) -> Result<ApiDefinitionIdAnnotated<CommonNamespace>, ApiRegistrationError> {
+        Ok(ApiDefinitionIdAnnotated {
+            namespace: CommonNamespace::default(),
+            api_definition: api_definition_id.clone()
+        })
     }
 
     async fn get_all(
