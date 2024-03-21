26088bfe8ea5c15fa716168ab555f0d14c3149cf
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-03-21 17:29:56 +0100

    Making client generation part of golem-client's build script (#291)
    * Making client generation part of golem-client's build script
    
    * empty lib.rs
    
    * Missing file
    
    * Fix crate version
    
    * Fix
    
    * CI fix

--- scripts/generate-golem-client.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -uo pipefail
-
-script_full_path=$(dirname "$0")
-
-client_version="${GOLEM_CLIENT_VERSION:-0.0.0-git}"
-
-cargo install golem-openapi-client-generator@0.0.1
-golem-openapi-client-generator generate --spec-yaml ${script_full_path}/../openapi/golem-service.yaml --output-directory ${script_full_path}/../golem-client --name "golem-client" --client-version "${client_version}"
