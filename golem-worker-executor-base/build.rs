5e4c758692eb1e1b23a4e84b202ebc5050e3973c
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-03-21 20:23:44 +0100

    Avoid creating symlink in src dir from build.rs

--- golem-worker-executor-base/build.rs
+++ golem-worker-executor-base/build.rs
@@ -1,19 +1,16 @@
 use cargo_metadata::MetadataCommand;
+use std::env::var_os;
 use std::path::Path;
-use symlink::{remove_symlink_dir, symlink_dir};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let golem_wit_root = find_package_root("golem-wit");
-    let target_root = Path::new("./golem-wit");
-    if target_root.exists() {
-        let _ = remove_symlink_dir(target_root);
-    }
-    println!(
-        "Creating symlink from {} to {}",
-        golem_wit_root,
-        target_root.display()
-    );
-    symlink_dir(golem_wit_root, target_root)?;
+    let out_dir = var_os("OUT_DIR").unwrap();
+    let target_file = Path::new(&out_dir).join("preview2_mod.rs");
+
+    std::fs::write(&target_file, preview2_mod_gen(&golem_wit_root)).unwrap();
+
+    println!("cargo::rerun-if-changed=build.rs");
+
     Ok(())
 }
 
@@ -24,4 +21,42 @@ fn find_package_root(name: &str) -> String {
         .unwrap();
     let package = metadata.packages.iter().find(|p| p.name == name).unwrap();
     package.manifest_path.parent().unwrap().to_string()
+}
+
+fn preview2_mod_gen(golem_wit_path: &str) -> String {
+    format!(
+        r#"wasmtime::component::bindgen!({{
+        path: "{golem_wit_path}/wit",
+        interfaces: "
+          import golem:api/host@0.2.0;
+
+          import wasi:blobstore/blobstore;
+          import wasi:blobstore/container;
+          import wasi:blobstore/types;
+          import wasi:keyvalue/atomic@0.1.0;
+          import wasi:keyvalue/eventual-batch@0.1.0;
+          import wasi:keyvalue/cache@0.1.0;
+          import wasi:keyvalue/eventual@0.1.0;
+          import wasi:keyvalue/types@0.1.0;
+          import wasi:keyvalue/wasi-keyvalue-error@0.1.0;
+          import wasi:logging/logging;
+        ",
+        tracing: false,
+        async: true,
+        with: {{
+            "wasi:io/streams/input-stream": InputStream,
+            "wasi:io/streams/output-stream": OutputStream,
+            "wasi:io/poll/pollable": Pollable,
+            "wasi:blobstore/container/container": super::durable_host::blobstore::types::ContainerEntry,
+            "wasi:blobstore/container/stream-object-names": super::durable_host::blobstore::types::StreamObjectNamesEntry,
+            "wasi:blobstore/types/incoming-value": super::durable_host::blobstore::types::IncomingValueEntry,
+            "wasi:blobstore/types/outgoing-value": super::durable_host::blobstore::types::OutgoingValueEntry,
+            "wasi:keyvalue/wasi-keyvalue-error/error": super::durable_host::keyvalue::error::ErrorEntry,
+            "wasi:keyvalue/types/bucket": super::durable_host::keyvalue::types::BucketEntry,
+            "wasi:keyvalue/types/incoming-value": super::durable_host::keyvalue::types::IncomingValueEntry,
+            "wasi:keyvalue/types/outgoing-value": super::durable_host::keyvalue::types::OutgoingValueEntry,
+        }}
+    }});
+        "#
+    )
 }
