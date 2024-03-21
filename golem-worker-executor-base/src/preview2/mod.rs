187f640ce1b908b6eaed54a97f251b0b12cdc628
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-03-21 23:05:59 +0100

    Avoid creating symlink in src dir from build.rs (#294)
    * Avoid creating symlink in src dir from build.rs
    
    * Clippy

--- golem-worker-executor-base/src/preview2/mod.rs
+++ golem-worker-executor-base/src/preview2/mod.rs
@@ -12,38 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-wasmtime::component::bindgen!({
-    path: "golem-wit/wit",
-    interfaces: "
-      import golem:api/host@0.2.0;
-
-      import wasi:blobstore/blobstore;
-      import wasi:blobstore/container;
-      import wasi:blobstore/types;
-      import wasi:keyvalue/atomic@0.1.0;
-      import wasi:keyvalue/eventual-batch@0.1.0;
-      import wasi:keyvalue/cache@0.1.0;
-      import wasi:keyvalue/eventual@0.1.0;
-      import wasi:keyvalue/types@0.1.0;
-      import wasi:keyvalue/wasi-keyvalue-error@0.1.0;
-      import wasi:logging/logging;
-    ",
-    tracing: false,
-    async: true,
-    with: {
-        "wasi:io/streams/input-stream": InputStream,
-        "wasi:io/streams/output-stream": OutputStream,
-        "wasi:io/poll/pollable": Pollable,
-        "wasi:blobstore/container/container": super::durable_host::blobstore::types::ContainerEntry,
-        "wasi:blobstore/container/stream-object-names": super::durable_host::blobstore::types::StreamObjectNamesEntry,
-        "wasi:blobstore/types/incoming-value": super::durable_host::blobstore::types::IncomingValueEntry,
-        "wasi:blobstore/types/outgoing-value": super::durable_host::blobstore::types::OutgoingValueEntry,
-        "wasi:keyvalue/wasi-keyvalue-error/error": super::durable_host::keyvalue::error::ErrorEntry,
-        "wasi:keyvalue/types/bucket": super::durable_host::keyvalue::types::BucketEntry,
-        "wasi:keyvalue/types/incoming-value": super::durable_host::keyvalue::types::IncomingValueEntry,
-        "wasi:keyvalue/types/outgoing-value": super::durable_host::keyvalue::types::OutgoingValueEntry,
-    }
-});
+include!(concat!(env!("OUT_DIR"), "/preview2_mod.rs"));
 
 pub type InputStream = wasmtime_wasi::preview2::InputStream;
 pub type OutputStream = wasmtime_wasi::preview2::OutputStream;
