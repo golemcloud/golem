7f0fef446262c6830d4d28ab9bf863eac6075990
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-03-22 19:17:09 +0100

    Publish fixes

--- golem-client/build.rs
+++ golem-client/build.rs
@@ -1,17 +1,41 @@
 use std::env::var_os;
-use std::path::Path;
+use std::ffi::OsString;
+use std::path::{Path, PathBuf};
 
 use golem_openapi_client_generator::parse_openapi_specs;
 
 fn main() {
     println!("Starting code generation for Golem OpenAPI client.");
     let out_dir = var_os("OUT_DIR").unwrap();
+    let manifest_dir = var_os("CARGO_MANIFEST_DIR").unwrap();
+
+    let yaml_path = Path::new(&manifest_dir).join("../openapi/golem-service.yaml");
 
     println!("Output directory: {:?}", out_dir);
+    println!("Workspace OpenAPI file: {:?}", yaml_path);
 
+    if yaml_path.exists() {
+        generate(yaml_path.clone(), out_dir);
+
+        // Copying the file to the crate so it gets packaged
+        std::fs::create_dir_all(Path::new(&manifest_dir).join("openapi")).unwrap();
+        std::fs::copy(
+            yaml_path.clone(),
+            Path::new(&manifest_dir).join("openapi/golem-service.yaml"),
+        )
+        .unwrap();
+
+        println!("cargo::rerun-if-changed=build.rs");
+        println!("cargo::rerun-if-changed={yaml_path:?}");
+    } else {
+        let crate_yaml_path = Path::new(&manifest_dir).join("openapi/golem-service.yaml");
+        generate(crate_yaml_path, out_dir);
+    }
+}
+
+fn generate(yaml_path: PathBuf, out_dir: OsString) {
     golem_openapi_client_generator::gen(
-        parse_openapi_specs(&[Path::new("../openapi/golem-service.yaml").to_path_buf()])
-            .expect("Failed to parse OpenAPI spec."),
+        parse_openapi_specs(&[yaml_path.clone()]).expect("Failed to parse OpenAPI spec."),
         Path::new(&out_dir),
         "golem-client",
         "0.0.0",
@@ -19,7 +43,4 @@ fn main() {
         true,
     )
     .expect("Failed to generate client code from OpenAPI spec.");
-
-    println!("cargo::rerun-if-changed=build.rs");
-    println!("cargo::rerun-if-changed=../openapi/golem-service.yaml");
 }
