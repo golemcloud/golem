efd632f8da0a22ce2c65b8a2e4f09c4de2e4ee0a
Nico Burniske <nicoburniske@gmail.com>
2024-03-22 12:04:41 +0100

    consistent file naming

--- golem-worker-service-base/src/service/worker/mod.rs
+++ golem-worker-service-base/src/service/worker/mod.rs
@@ -1,9 +1,9 @@
-mod connect;
+mod connect_proxy;
+mod connect_stream;
 mod default;
 mod error;
-mod proxy_connect;
 
-pub use connect::*;
+pub use connect_proxy::*;
+pub use connect_stream::*;
 pub use default::*;
 pub use error::*;
-pub use proxy_connect::*;
