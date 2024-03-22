dcca82554f8282c688472c1c73c1e2763973fcf8
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-03-22 08:56:02 +0100

    Proper way to generate code from build.rs (#1358)
    * Update to latest golem-sevices and use K8s healthcheck in shard manager
    
    * Fix helm chart
    
    * Proper way to generate code from build.rs
    
    * 0.0.72
    
    * Follow changes

--- golem-cloud-client/build.rs
+++ golem-cloud-client/build.rs
@@ -1,14 +1,24 @@
 use golem_openapi_client_generator::parse_openapi_specs;
+use std::env::var_os;
 use std::path::Path;
 
 fn main() {
+    println!("Starting code generation for Golem Cloud OpenAPI client.");
+    let out_dir = var_os("OUT_DIR").unwrap();
+
+    println!("Output directory: {:?}", out_dir);
+
     golem_openapi_client_generator::gen(
         parse_openapi_specs(&[Path::new("../openapi/cloud-spec.yaml").to_path_buf()])
             .expect("Failed to parse OpenAPI spec."),
-        Path::new("."),
+        Path::new(&out_dir),
         "golem-cloud-client",
         "0.0.0",
         false,
+        true,
     )
-    .expect("Failed to generate client code from OpenAPI spec.")
+    .expect("Failed to generate client code from OpenAPI spec.");
+
+    println!("cargo::rerun-if-changed=build.rs");
+    println!("cargo::rerun-if-changed=../openapi/cloud-spec.yaml");
 }
