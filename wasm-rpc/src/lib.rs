86abacb06504ca6f52daec9f4ccf9b2220f061de
Afsal Thaj <afsal.taj06@gmail.com>
2024-03-26 07:07:12 +1100

    Fix compiler warnings

--- wasm-rpc/src/lib.rs
+++ wasm-rpc/src/lib.rs
@@ -589,12 +589,13 @@ impl WasmValue for TypeAnnotatedValue {
         val: Option<Self>,
     ) -> Result<Self, WasmValueError> {
         if let golem_wasm_ast::analysis::AnalysedType::Variant(cases) = &ty.0 {
-            let case_type = cases
-                .iter()
-                .enumerate()
-                .find_map(
-                    |(idx, (name, case_type))| if name == case { Some(case_type) } else { None },
-                );
+            let case_type = cases.iter().enumerate().find_map(|(_, (name, case_type))| {
+                if name == case {
+                    Some(case_type)
+                } else {
+                    None
+                }
+            });
             if case_type.is_some() {
                 Ok(TypeAnnotatedValue::Variant(VariantValue {
                     typ: cases
