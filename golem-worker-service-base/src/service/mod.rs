64f0a067ce64fc4b5a2bb31504e5999cedc8e0cf
Nico Burniske <46934294+nicoburniske@users.noreply.github.com>
2024-03-28 11:00:58 -0400

    Remove AuthService and Namespace (#332)
    * Revert "Revert remove namespace (#331)"
    
    This reverts commit c499e618eb6cc2ccdcb25d9ee6713cc4868a36d5.
    
    * implement thiserror for GolemError

--- golem-worker-service-base/src/service/mod.rs
+++ golem-worker-service-base/src/service/mod.rs
@@ -3,3 +3,23 @@ pub mod api_definition_validator;
 pub mod http_request_definition_lookup;
 pub mod template;
 pub mod worker;
+
+pub fn with_metadata<T, I, K, V>(request: T, metadata: I) -> tonic::Request<T>
+where
+    I: IntoIterator<Item = (K, V)>,
+    K: AsRef<str>,
+    V: AsRef<str>,
+{
+    let mut req = tonic::Request::new(request);
+    let req_metadata = req.metadata_mut();
+
+    for (key, value) in metadata {
+        let key = tonic::metadata::MetadataKey::from_bytes(key.as_ref().as_bytes());
+        let value = value.as_ref().parse();
+        if let (Ok(key), Ok(value)) = (key, value) {
+            req_metadata.insert(key, value);
+        }
+    }
+
+    req
+}
--- golem-worker-service-base/src/service/worker/error.rs
+++ golem-worker-service-base/src/service/worker/error.rs
@@ -2,19 +2,12 @@ use golem_api_grpc::proto::golem::worker::{
     worker_error, worker_execution_error, UnknownError, WorkerError as GrpcWorkerError,
 };
 use golem_common::model::{AccountId, TemplateId, WorkerId};
-use golem_service_base::{
-    model::{GolemError, VersionedTemplateId},
-    service::auth::AuthError,
-};
+use golem_service_base::model::{GolemError, VersionedTemplateId};
 
 use crate::service::template::TemplateServiceError;
 
 #[derive(Debug, thiserror::Error)]
 pub enum WorkerServiceError {
-    #[error(transparent)]
-    Auth(#[from] AuthError),
-    #[error("Internal error: {0}")]
-    Internal(#[from] anyhow::Error),
     #[error(transparent)]
     Template(#[from] TemplateServiceError),
     // TODO: This should prob be a vec?
@@ -26,11 +19,11 @@ pub enum WorkerServiceError {
     TemplateNotFound(TemplateId),
     #[error("Account not found: {0}")]
     AccountIdNotFound(AccountId),
-    // TODO: Once worker is independent of account
     #[error("Worker not found: {0}")]
     WorkerNotFound(WorkerId),
-    // TODO: Fix display impl.
-    #[error("Golem error")]
+    #[error("Internal error: {0}")]
+    Internal(#[from] anyhow::Error),
+    #[error(transparent)]
     Golem(GolemError),
 }
 
@@ -57,24 +50,6 @@ impl From<WorkerServiceError> for worker_error::Error {
         use golem_api_grpc::proto::golem::worker::WorkerExecutionError;
 
         match error {
-            WorkerServiceError::Auth(error) => match error {
-                AuthError::Unauthorized(_) => worker_error::Error::Unauthorized(ErrorBody {
-                    error: error.to_string(),
-                }),
-                AuthError::Forbidden(_) => worker_error::Error::LimitExceeded(ErrorBody {
-                    error: error.to_string(),
-                }),
-                AuthError::NotFound(_) => worker_error::Error::NotFound(ErrorBody {
-                    error: error.to_string(),
-                }),
-                AuthError::Internal(_) => {
-                    worker_error::Error::InternalError(WorkerExecutionError {
-                        error: Some(worker_execution_error::Error::Unknown(UnknownError {
-                            details: error.to_string(),
-                        })),
-                    })
-                }
-            },
             error @ (WorkerServiceError::TemplateNotFound(_)
             | WorkerServiceError::AccountIdNotFound(_)
             | WorkerServiceError::VersionedTemplateIdNotFound(_)
