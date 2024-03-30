c8ebe68cc4db55701050d08b747b685d21f835f6
Afsal Thaj <afsal.taj06@gmail.com>
2024-03-31 07:14:04 +1100

    Remove comments

--- golem-worker-service-base/src/getter.rs
+++ golem-worker-service-base/src/getter.rs
@@ -14,8 +14,6 @@ pub enum GetError {
     NotRecord { key_name: String, found: String },
     NotArray { index: usize, found: String },
 }
-
-// TypeAnnotatedValue doesn't have a display instance, therefore no generic display impl
 impl Display for GetError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
