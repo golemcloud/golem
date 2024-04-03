a8b4213e9807c6beb3664c831f49b6fe3aa87561
Nico Burniske <nicoburniske@gmail.com>
2024-04-03 08:42:37 -0400

    add to grpc server

--- golem-worker-service/src/grpcapi/mod.rs
+++ golem-worker-service/src/grpcapi/mod.rs
@@ -1,8 +1,10 @@
 use golem_api_grpc::proto;
+use golem_api_grpc::proto::golem::apidefinition::api_definition_registration_service_server::ApiDefinitionRegistrationServiceServer;
 use golem_api_grpc::proto::golem::worker::worker_service_server::WorkerServiceServer;
 use std::net::SocketAddr;
 use tonic::transport::{Error, Server};
 
+use crate::grpcapi::register_api_definition::GrpcApiDefinitionRegistration;
 use crate::grpcapi::worker::WorkerGrpcApi;
 use crate::service::Services;
 
@@ -16,6 +18,10 @@ pub async fn start_grpc_server(addr: SocketAddr, services: &Services) -> Result<
         .set_serving::<WorkerServiceServer<WorkerGrpcApi>>()
         .await;
 
+    health_reporter
+        .set_serving::<ApiDefinitionRegistrationServiceServer<GrpcApiDefinitionRegistration>>()
+        .await;
+
     let reflection_service = tonic_reflection::server::Builder::configure()
         .register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
         .build()
@@ -24,10 +30,13 @@ pub async fn start_grpc_server(addr: SocketAddr, services: &Services) -> Result<
     Server::builder()
         .add_service(reflection_service)
         .add_service(health_service)
-        .add_service(WorkerServiceServer::new(WorkerGrpcApi {
-            template_service: services.template_service.clone(),
-            worker_service: services.worker_service.clone(),
-        }))
+        .add_service(WorkerServiceServer::new(WorkerGrpcApi::new(
+            services.template_service.clone(),
+            services.worker_service.clone(),
+        )))
+        .add_service(ApiDefinitionRegistrationServiceServer::new(
+            GrpcApiDefinitionRegistration::new(services.definition_service.clone()),
+        ))
         .serve(addr)
         .await
 }
