06d44c6fd738aff884fb6f95c4cd0a2f9e0f54f8
Vilmos Feher <jupposessho@gmail.com>
2024-04-04 19:32:05 +0200

    Expose api-definition endpoints to golem-cli

--- golem-cli/src/lib.rs
+++ golem-cli/src/lib.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub mod api_definition;
 pub mod clients;
 pub mod examples;
 pub mod model;
