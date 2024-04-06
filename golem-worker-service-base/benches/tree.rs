f2a2a8bb805a34f76fe1ef6a12d6946822ddd141
Nico Burniske <nicoburniske@gmail.com>
2024-04-06 17:11:25 -0400

    improve tree benchmark

--- golem-worker-service-base/benches/tree.rs
+++ golem-worker-service-base/benches/tree.rs
@@ -1,70 +1,121 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use golem_worker_service_base::http::tree::{make_path, Pattern, RadixNode};
+use golem_worker_service_base::http::tree::{make_path, LiteralPattern, Pattern, RadixNode};
 
 criterion_group!(benches, radix_tree_all_matches);
 criterion_main!(benches);
 
+const NUM_ROUTES: &[usize] = &[10, 20, 50, 100];
+const HIT_RATES: &[u8] = &[25, 50, 75];
+
 fn radix_tree_all_matches(c: &mut Criterion) {
-    let num_routes = &[10, 20, 50, 100];
-
     let mut group = c.benchmark_group("matches");
-    for &len in num_routes.iter() {
-        group.bench_function(format!("/len={len}"), |b| {
-            let (original, routes) = generate_routes(len);
-            let radix_tree = build_radix_tree(&routes);
+    for &hit_rate in HIT_RATES.iter() {
+        for &len in NUM_ROUTES.iter() {
+            group.bench_function(format!("len={len}/hit={hit_rate}"), |b| {
+                let routes = generate_routes(len);
+                let radix_tree = build_radix_tree(&routes);
 
-            let match_routes: Vec<_> = (0..1000).map(|_| generate_match_route(&original)).collect();
+                let (match_routes, miss_routes) = build_routes(&radix_tree);
 
-            b.iter_with_setup(
-                || {
-                    let route = fastrand::choice(&match_routes).unwrap();
-                    let refs = route.iter().map(|s| s.as_str()).collect::<Vec<_>>();
-                    black_box(refs)
-                },
-                |refs| {
-                    let _ = radix_tree.matches(refs.as_slice()).unwrap();
-                },
-            );
-        });
+                b.iter_with_setup(
+                    || {
+                        let hit = fastrand::u8(0..100) < hit_rate;
+                        let route = if hit {
+                            fastrand::choice(&match_routes).unwrap()
+                        } else {
+                            fastrand::choice(&miss_routes).unwrap()
+                        };
+                        let refs = route.iter().map(|s| s.as_str()).collect::<Vec<_>>();
+                        black_box((hit, refs))
+                    },
+                    |(hit, refs)| {
+                        let result = radix_tree.matches(refs.as_slice());
+                        if hit {
+                            assert!(result.is_some(), "{result:?} {refs:?}");
+                        } else {
+                            assert!(result.is_none(), "{result:?} {refs:?}");
+                        }
+                    },
+                );
+            });
+        }
     }
     group.finish();
 }
 
-fn generate_routes(n: usize) -> (Vec<&'static str>, Vec<Vec<Pattern>>) {
-    let mut original_routes = Vec::with_capacity(n);
+fn generate_routes(n: usize) -> Vec<Vec<Pattern>> {
+    let mut result = Vec::with_capacity(n);
     let mut used_routes = std::collections::HashSet::with_capacity(n);
 
-    while original_routes.len() < n {
+    while result.len() < n {
         let route = *fastrand::choice(ROUTES).unwrap();
         if !used_routes.contains(route) {
-            original_routes.push(route);
+            result.push(make_path(route));
             used_routes.insert(route);
         }
     }
 
-    let patterns = original_routes.iter().map(|s| make_path(s)).collect();
-    (original_routes, patterns)
+    result
 }
 
-fn build_radix_tree(routes: &[Vec<Pattern>]) -> RadixNode<usize> {
+fn build_radix_tree(routes: &[Vec<Pattern>]) -> RadixNode<(usize, String)> {
     let mut radix_tree = RadixNode::default();
     for (index, route) in routes.iter().enumerate() {
-        radix_tree.insert_path(route, index).unwrap();
+        let display_route = route
+            .iter()
+            .map(|segment| match segment {
+                Pattern::Literal(LiteralPattern(literal)) => literal.clone(),
+                Pattern::Variable => "{var}".to_string(),
+                Pattern::CatchAll => "*".to_string(),
+            })
+            .collect::<Vec<_>>()
+            .join("/");
+
+        let data = (index, display_route);
+        radix_tree.insert_path(route, data).unwrap();
     }
     radix_tree
 }
 
-fn generate_match_route(routes: &[&str]) -> Vec<String> {
+fn build_routes<T>(router: &RadixNode<T>) -> (Vec<Vec<String>>, Vec<Vec<String>>) {
+    let mut match_routes = Vec::new();
+    let mut miss_routes = Vec::new();
+
+    let all_patterns = ROUTES.iter().map(|s| make_path(s)).collect::<Vec<_>>();
+
+    loop {
+        let need_matches = match_routes.len() < 500;
+        let need_misses = miss_routes.len() < 100;
+
+        if !need_matches && !need_misses {
+            break;
+        }
+
+        let route = generate_match_route(&all_patterns);
+        let route_str = route.iter().map(|s| s.as_str()).collect::<Vec<_>>();
+
+        if router.matches(&route_str).is_some() {
+            if need_matches {
+                match_routes.push(route);
+            }
+        } else if need_misses {
+            miss_routes.push(route);
+        }
+    }
+
+    (match_routes, miss_routes)
+}
+
+fn generate_match_route(routes: &[Vec<Pattern>]) -> Vec<String> {
     let route = fastrand::choice(routes).unwrap();
     route
-        .trim_matches('/')
-        .split('/')
-        .map(|segment| {
-            if segment.starts_with(':') {
-                fastrand::u32(1..1000).to_string()
-            } else {
-                segment.to_string()
-            }
+        .iter()
+        .flat_map(|segment| match segment {
+            Pattern::Literal(LiteralPattern(literal)) => vec![literal.clone()],
+            Pattern::Variable => vec![fastrand::u32(1..1000).to_string()],
+            Pattern::CatchAll => (1..10)
+                .map(|_| fastrand::u32(1..1000).to_string())
+                .collect::<Vec<_>>(),
         })
         .collect::<Vec<_>>()
 }
@@ -104,8 +155,8 @@ const ROUTES: &[&str] = &[
     "/api/v2/users/:id/timeline",
     "/api/v2/posts",
     "/api/v2/posts/trending",
-    "/assets/*path",
-    "/static/*filepath",
+    "/assets/path/:asset_id",
+    "/static/filepath/:file_id",
     "/admin",
     "/admin/users",
     "/admin/users/:id",
