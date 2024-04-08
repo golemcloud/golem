702e3054b765b4128a0d7598d784fdedb2c3ef90
Afsal Thaj <afsal.taj06@gmail.com>
2024-04-08 18:14:06 +1000

    Fix comments

--- golem-worker-service-base/src/wasm_wave_read.rs
+++ golem-worker-service-base/src/wasm_wave_read.rs
@@ -3,24 +3,24 @@ use crate::expression;
 use golem_wasm_rpc::{TypeAnnotatedValue, Value};
 
 // A handy utility that allows you to read string of wasm_syntax to wasm value (golem_wasm_rpc::value::Value)
-// without explicitly passing `AnalysedType` unlike wasm_wave functionalities.
+// without explicitly passing `AnalysedType` unlike wasm_wave functionalities. It may not be used in the wild.
 // Note that,it is 90% compatible with wasm_wave syntax.
 // The progression of this utility is to be fully compatible with wasm_wave syntax.
 // The minor differences are every wasm syntax needs to be wrapped in a `${}` to work with this utility.
 // Another difference, expr_parser currently uses `single quotes` for literals but wasm-wave uses `double-quotes`
 // Not to mention wasm-wave syntax doesn't allow undefined variables such `request.path`, but those are features exclusive to `Expr`.
 // In short, `Expr` is a superset of wasm_wave syntax, and we use same expression parser to read wasm_syntax string, and convert to `TypeAnnotatedValue`
-pub fn read_wasm_wave_string(wasm_syntax_str: impl AsRef<str>) -> Result<Value, String> {
-    let typed_value = read_wasm_wave_string_partially_typed(wasm_syntax_str)?;
+pub fn read_wasm_wave_string(wave_syntax_str: impl AsRef<str>) -> Result<Value, String> {
+    let typed_value = read_wasm_wave_string_internal(wave_syntax_str)?;
     Value::try_from(typed_value).map_err(|e| e.to_string())
 }
 
 // This is partially typed because, we are not passing `AnalysedType` here
 // especially for Variants, Enums and Flags.
-fn read_wasm_wave_string_partially_typed(
-    wasm_syntax_str: impl AsRef<str>,
+fn read_wasm_wave_string_internal(
+    wave_syntax_str: impl AsRef<str>,
 ) -> Result<TypeAnnotatedValue, String> {
-    let expr = expression::from_string(wasm_syntax_str).map_err(|e| e.to_string())?;
+    let expr = expression::from_string(wave_syntax_str).map_err(|e| e.to_string())?;
     expr.evaluate(&TypeAnnotatedValue::Record {
         typ: vec![],
         value: vec![],
