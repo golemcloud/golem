3ff8ff0f8377781964d63febcafe1c57047989cd
Afsal Thaj <afsal.taj06@gmail.com>
2024-04-10 21:36:10 +1000

    Fix expr text module

--- golem-worker-service-base/src/parser/expr/constructor.rs
+++ golem-worker-service-base/src/parser/expr/constructor.rs
@@ -1,6 +1,5 @@
 use crate::expression::{ConstructorPattern, Expr};
-use crate::parser::expr_parser::parse_code;
-use crate::parser::ParseError;
+use crate::parser::{expr_parser::parse_code, ParseError};
 use crate::tokeniser::tokenizer::{Token, Tokenizer};
 
 // To parse expressions such as some(x), foo(bar)
@@ -10,34 +9,17 @@ pub(crate) fn get_constructor_pattern(
 ) -> Result<ConstructorPattern, ParseError> {
     match tokenizer.next_non_empty_token() {
         Some(Token::LParen) => {
-            let constructor_var_optional =
-                tokenizer.capture_string_until_and_skip_end(&Token::RParen);
-            match constructor_var_optional {
-                Some(construction_variables) => {
-                    if construction_variables.is_empty() {
-                        ConstructorPattern::constructor(
-                            constructor_name.to_string().as_str(),
-                            vec![],
-                        )
-                    } else {
-                        let construction_variables =
-                            collect_construction_variables(construction_variables.as_str())?;
-                        let constructor_patterns = construction_variables
-                            .iter()
-                            .map(|expr| ConstructorPattern::Literal(Box::new(expr.clone())))
-                            .collect::<Vec<ConstructorPattern>>();
-
-                        ConstructorPattern::constructor(constructor_name, constructor_patterns)
-                    }
-                }
-                None => Err(ParseError::Message(format!(
-                    "Empty value inside the constructor {}",
-                    constructor_name
-                ))),
-            }
+            let construction_variables = match tokenizer.capture_string_until_and_skip_end(&Token::RParen) {
+                Some(value) => collect_construction_variables(value.as_str())?,
+                None => return Err(ParseError::Message(format!("Empty value inside the constructor {}", constructor_name))),
+            };
+            let constructor_patterns = construction_variables
+                .into_iter()
+                .map(|expr| ConstructorPattern::Literal(Box::new(expr)))
+                .collect();
+            ConstructorPattern::constructor(constructor_name, constructor_patterns)
         }
-
-        _ => ConstructorPattern::constructor(constructor_name.to_string().as_str(), vec![]),
+        _ => ConstructorPattern::constructor(constructor_name, vec![]),
     }
 }
 
@@ -46,18 +28,14 @@ fn collect_construction_variables(constructor_variable_str: &str) -> Result<Vec<
     let mut construction_variables = vec![];
     loop {
         if let Some(value) = tokenizer.capture_string_until_and_skip_end(&Token::Comma) {
-            let construction_variable = parse_code(value.as_str())?;
-            construction_variables.push(construction_variable);
+            construction_variables.push(parse_code(value.as_str())?);
         } else {
             let rest = tokenizer.rest();
-
             if !rest.is_empty() {
-                let construction_variable = parse_code(rest)?;
-                construction_variables.push(construction_variable);
+                construction_variables.push(parse_code(rest)?);
             }
             break;
         }
     }
-
     Ok(construction_variables)
 }
