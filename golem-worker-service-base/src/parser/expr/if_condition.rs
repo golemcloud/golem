f479c4a5db6f5a73b3f8c5ceb5dc280090509ff5
Afsal Thaj <afsal.taj06@gmail.com>
2024-04-10 16:15:39 +1000

    Clean up if condition

--- golem-worker-service-base/src/parser/expr/if_condition.rs
+++ golem-worker-service-base/src/parser/expr/if_condition.rs
@@ -3,46 +3,36 @@ use crate::parser::expr_parser::parse_code;
 use crate::parser::ParseError;
 use crate::tokeniser::tokenizer::{Token, Tokenizer};
 
-// Assuming if token is already consumed
 pub(crate) fn create_if_condition(tokenizer: &mut Tokenizer) -> Result<Expr, ParseError> {
-    if let Some(predicate_str) =
-        tokenizer.capture_string_until_and_skip_end(&Token::then())
-    {
-        let predicate_expr = parse_code(predicate_str.as_str())?;
-        dbg!(&predicate_expr);
-        let then_str = tokenizer.capture_string_until_and_skip_end(&Token::else_token());
-        match then_str {
-            Some(then_str) => {
-                let then_expr = parse_code(then_str.as_str())?;
+    // Parse the predicate expression
+    let predicate_str = tokenizer
+        .capture_string_until_and_skip_end(&Token::then())
+        .ok_or_else(|| ParseError::Message("Expecting a valid expression after if".to_string()))?;
 
-                dbg!(&then_expr);
+    let predicate_expr = parse_code(predicate_str.as_str())?;
 
-                let else_condition = tokenizer
-                    .capture_string_until_and_skip_end(&Token::SemiColon)
-                    .or_else(|| Some(tokenizer.consume_rest().to_string()));
+    // Parse the 'then' branch expression
+    let then_str = tokenizer
+        .capture_string_until_and_skip_end(&Token::else_token())
+        .ok_or_else(|| ParseError::Message("Expecting a valid expression after then".to_string()))?;
 
-                match else_condition {
-                    Some(else_condition) => {
-                        let else_expr = parse_code(else_condition.as_str())?;
-                        dbg!(&else_expr);
-                        Ok(Expr::Cond(
-                            Box::new(predicate_expr),
-                            Box::new(then_expr),
-                            Box::new(else_expr),
-                        ))
-                    }
-                    None => Err(ParseError::Message(
-                        "Expecting a valid expression after then".to_string(),
-                    )),
-                }
-            }
-            None => Err(ParseError::Message(
-                "Expecting a valid expression after then".to_string(),
-            )),
-        }
-    } else {
-        Err(ParseError::Message(
-            "Expecting a valid expression after if".to_string(),
-        ))
-    }
+    let then_expr = parse_code(then_str.as_str())?;
+
+    // Parse the 'else' branch expression
+    let else_condition = tokenizer
+        .capture_string_until_and_skip_end(&Token::SemiColon)
+        .or_else(|| Some(tokenizer.consume_rest().to_string()));
+
+    let else_expr = match else_condition {
+        Some(else_condition) => parse_code(else_condition.as_str())?,
+        None => return Err(ParseError::Message("Expecting a valid expression after then".to_string())),
+    };
+
+
+    // Return the conditional expression
+    Ok(Expr::Cond(
+        Box::new(predicate_expr),
+        Box::new(then_expr),
+        Box::new(else_expr),
+    ))
 }
