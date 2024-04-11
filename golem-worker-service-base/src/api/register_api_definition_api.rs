6a10035e36da9e0434f93e807b91b5dba036a845
Afsal Thaj <afsal.taj06@gmail.com>
2024-04-11 20:26:40 +1000

    Fix to_string

--- golem-worker-service-base/src/api/register_api_definition_api.rs
+++ golem-worker-service-base/src/api/register_api_definition_api.rs
@@ -283,8 +283,7 @@ impl TryFrom<crate::worker_binding::GolemWorkerBinding> for grpc_apidefinition::
     fn try_from(value: crate::worker_binding::GolemWorkerBinding) -> Result<Self, Self::Error> {
         let response: Option<String> = match value.response {
             Some(v) => {
-                let r = v.0.to_string().map_err(|e| e.to_string())?;
-                Some(r)
+                Some(v.0.to_string())
             }
             None => None,
         };
