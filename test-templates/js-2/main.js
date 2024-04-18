6ab30fbf572e75e27ecbd1c139ccde3c56a7c9c7
Nico Burniske <46934294+nicoburniske@users.noreply.github.com>
2024-04-18 12:02:47 -0400

    Update JS Templates (#416)
    * update js components and tests
    
    * add wasm files for js test templates
    
    * update js-3 to test timeout behavior
    
    * update setTimeout test
    
    * remove componentize.js files
    
    * update js-3 to use typescript
    
    * remove unused main.js

--- test-templates/js-2/main.js
+++ test-templates/js-2/main.js
@@ -1,11 +1,11 @@
 var state = 0;
 
 export const api = {
-    "add": function (value) {
+    add(value) {
         console.log(`Adding ${value} to the counter`);
         state += Number(value);
     },
-    "get": function() {
+    get() {
         console.log(`Returning the current counter value: ${state}`);
         return BigInt(state);
     }
