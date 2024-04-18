a38b72e4aaea0fadaeb13de001d41a403bca5fbc
Vilmos Feher <jupposessho@gmail.com>
2024-04-18 13:31:21 +0200

    Add benchmarks with cold start

--- integration-tests/src/benchmarks/cold_start_large.rs
+++ integration-tests/src/benchmarks/cold_start_large.rs
@@ -13,23 +13,21 @@
 // limitations under the License.
 
 use async_trait::async_trait;
-use golem_wasm_rpc::Value;
 
 use golem_test_framework::config::{CliParams, TestDependencies};
 use golem_test_framework::dsl::benchmark::{Benchmark, BenchmarkRecorder};
-use golem_test_framework::dsl::TestDsl;
 use integration_tests::{run_benchmark, run_echo, setup, Context};
 
-struct SimpleWorkerEcho {
+struct ColdStartEchoLarge {
     config: CliParams,
 }
 
 #[async_trait]
-impl Benchmark for SimpleWorkerEcho {
+impl Benchmark for ColdStartEchoLarge {
     type IterationContext = Context;
 
     fn name() -> &'static str {
-        "simple-worker-echo"
+        "cold-start-large"
     }
 
     async fn create(config: CliParams) -> Self {
@@ -37,37 +35,10 @@ impl Benchmark for SimpleWorkerEcho {
     }
 
     async fn setup_iteration(&self) -> Self::IterationContext {
-        setup(self.config.clone(), "option-service").await
+        setup(self.config.clone(), "python-1").await
     }
 
-    async fn warmup(&self, context: &Self::IterationContext) {
-        // Invoke each worker a few times in parallel
-        let mut fibers = Vec::new();
-        for worker_id in &context.worker_ids {
-            let context_clone = context.clone();
-            let worker_id_clone = worker_id.clone();
-            let fiber = tokio::task::spawn(async move {
-                for _ in 0..5 {
-                    context_clone
-                        .deps
-                        .invoke_and_await(
-                            &worker_id_clone,
-                            "golem:it/api/echo",
-                            vec![Value::Option(Some(Box::new(Value::String(
-                                "hello".to_string(),
-                            ))))],
-                        )
-                        .await
-                        .expect("invoke_and_await failed");
-                }
-            });
-            fibers.push(fiber);
-        }
-
-        for fiber in fibers {
-            fiber.await.expect("fiber failed");
-        }
-    }
+    async fn warmup(&self, _: &Self::IterationContext) {}
 
     async fn run(&self, context: &Self::IterationContext, recorder: BenchmarkRecorder) {
         run_echo(self.config.benchmark_config.length, context, recorder).await
@@ -80,5 +51,5 @@ impl Benchmark for SimpleWorkerEcho {
 
 #[tokio::main]
 async fn main() {
-    run_benchmark::<SimpleWorkerEcho>().await;
+    run_benchmark::<ColdStartEchoLarge>().await;
 }
