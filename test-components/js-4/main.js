0eeef57273557e0befc5096730050c9666e4a386
Nico Burniske <nicoburniske@gmail.com>
2024-04-23 08:00:50 -0400

    move from templates to components

--- test-components/js-4/main.js
+++ test-components/js-4/main.js
@@ -1,15 +1,9 @@
-import { golemCreatePromise } from "wasi:golem/golem:api@0.2.0";
-
-var state = 0;
+import { golemCreatePromise } from "golem:api/host@0.2.0";
 
 export const api = {
-    add(value) {
+    createPromise() {
         let promiseId = golemCreatePromise();
-        console.log(`Adding ${value} to the counter`, promiseId);
-        state += Number(value);
-    },
-    get() {
-        console.log(`Returning the current counter value: ${state}`);
-        return BigInt(state);
+        console.log('Created Promise', promiseId);
+        return promiseId
     }
 }
