2fde5e7fb2570172bd1f777154fca9b952eb0a86
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-05-08 22:09:58 +0200

    Tests for tier-1 zig (#488)

--- test-components/zig-1/src/main.zig
+++ test-components/zig-1/src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
-pub fn main() anyerror!void {
+export fn zig1_run() void {
     const stdin = std.io.getStdIn().reader();
     const stdout = std.io.getStdOut().writer();
     const stderr = std.io.getStdErr().writer();
@@ -10,15 +10,18 @@ pub fn main() anyerror!void {
     var buf: [100]u8 = undefined;
 
     // Reading parameters from the standard input
-    if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |input| {
+    if (stdin.readUntilDelimiterOrEof(buf[0..], '\n') catch unreachable) |input| {
         if (std.fmt.parseInt(u64, input, 10)) |value| {
-            try stderr.print("Returning 2 * {}", .{value});
+            stderr.print("Returning 2 * {}", .{value}) catch unreachable;
 
             const result = 2 * value;
             // Writing the result to the standard output
-            try stdout.print("{}", .{result});
+            stdout.print("{}", .{result}) catch unreachable;
         } else |err| {
-            try stderr.print("Input {s} is not a number: {}", .{ input, err });
+            stderr.print("Input {s} is not a number: {}", .{ input, err }) catch unreachable;
         }
     }
+}
+
+pub fn main() anyerror!void {
 }
