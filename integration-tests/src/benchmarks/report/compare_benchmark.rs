c5dd89d40ebcfad4d0354162432ff1b5a9c776fa
Afsl Thaj <afsal.taj06@gmail.com>
2024-06-05 00:22:45 +1000

    Comparison workflow

--- integration-tests/src/benchmarks/report/compare_benchmark.rs
+++ integration-tests/src/benchmarks/report/compare_benchmark.rs
@@ -36,13 +36,24 @@ fn calculate_mean_avg_time(json: &BenchmarkResult) -> HashMap<RunConfig, u64> {
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 struct ComparisonResults {
-    result: Vec<ComparisonResult>
+    results: Vec<ComparisonResult>
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 struct ComparisonResult {
     run_config: RunConfig,
     comparison: Comparison
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct StandaloneResults {
+    results: Vec<StandaloneResult>
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct StandaloneResult {
+    run_config: RunConfig,
+    avg_time: u64
 }
 
 impl ComparisonResults {
@@ -66,7 +77,7 @@ impl ComparisonResults {
         }
 
         ComparisonResults {
-            result: comparison_results
+            results: comparison_results
         }
     }
 }
@@ -81,25 +92,59 @@ struct Comparison {
 #[command()]
 pub struct CliParams {
     #[arg(long)]
-    pub benchmark_previous: String,
+    pub benchmark_previous: Option<String>,
     #[arg(long)]
-    pub benchmark_current: String,
+    pub benchmark_current: String
+}
 
+enum BenchmarkReport {
+    Single(StandaloneResults),
+    ComparisonResult(ComparisonResults),
+}
+
+impl BenchmarkReport {
+    fn from_benchmark_result(benchmark_result: BenchmarkResult) -> Self {
+        let avg_times = calculate_mean_avg_time(&benchmark_result);
+        let standalone_results = avg_times.iter().map(|(run_config, avg_time)| {
+            StandaloneResult {
+                run_config: run_config.clone(),
+                avg_time: avg_time.clone()
+            }
+        }).collect();
+
+        BenchmarkReport::Single(StandaloneResults {
+            results: standalone_results
+        })
+    }
+
+    fn from_results(previous: &BenchmarkResult, current: &BenchmarkResult) -> Self {
+        let comparison_result = ComparisonResults::from_results(previous, current);
+        BenchmarkReport::ComparisonResult(comparison_result)
+    }
 }
 
 // The entry point to compare two benchmark results.
 // The files have contents expected to be in json format that correspond to `BenchmarkResult`.
 fn main() -> Result<(), Box<dyn Error>> {
     let params = CliParams::parse();
-    let previous_bench_mark_results = load_json(params.benchmark_previous.as_str())?;
+    let previous_bench_mark_results = params.benchmark_previous.map(|prev|load_json(prev.as_str())?);
     let current_bench_mark_results = load_json(params.benchmark_current.as_str())?;
 
-    let comparison_result =
-        ComparisonResults::from_results(&previous_bench_mark_results, &current_bench_mark_results);
+    let report = if let Some(prev) = previous_bench_mark_results {
+        BenchmarkReport::from_results(&prev, &current_bench_mark_results)
 
-    dbg!(comparison_result.clone());
+    } else {
+        BenchmarkReport::from_benchmark_result(current_bench_mark_results)
+    };
 
-    println!("{}", serde_json::to_string(&comparison_result)?);
+    match report {
+        BenchmarkReport::Single(standalone_results) => {
+            println!("{}", serde_json::to_string(&standalone_results)?);
+        },
+        BenchmarkReport::ComparisonResult(comparison_results) => {
+            println!("{}", serde_json::to_string(&comparison_results)?);
+        }
+    }
 
     Ok(())
 }
