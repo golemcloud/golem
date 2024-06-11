fc195fe3ba06b82d0f6140d2512a3bcf1fe58d63
Nico Burniske <nicoburniske@gmail.com>
2024-06-11 10:32:14 -0400

    remove nested directory for lib

--- ts-sdk/test/src/interfaces/golem-it-api.d.ts
+++
@@ -1,6 +0,0 @@
-export namespace GolemItApi {
-  export function failWithCustomMaxRetries(maxRetries: number): void;
-  export function explicitCommit(replicas: number): void;
-  export function fallibleTransactionTest(): boolean;
-  export function infallibleTransactionTest(): number;
-}
