7195d5c90559bbe8df539022398471466f361e6c
Afsl Thaj <afsal.taj06@gmail.com>
2024-06-25 01:47:48 +1000

    Fix stack overflow in select field

--- golem-rib/src/rib/parser/select_field.rs
+++ golem-rib/src/rib/parser/select_field.rs
@@ -1,62 +1,59 @@
+use combine::parser::char::{char as char_, letter, spaces, digit};
 use combine::stream::easy;
-use combine::{Stream};
+use combine::{attempt, choice, many1, optional, Parser, Stream};
+
 use crate::rib::expr::Expr;
+use crate::rib::parser::record::record;
 use combine::parser;
 
-// Intention was to support nested selected field - which stack overflows unlike Scala
 parser! {
     pub(crate) fn select_field['t]()(easy::Stream<&'t str>) -> Expr
-    where [easy::Stream<&'t str>: Stream<Token = char>,]{
+    where [easy::Stream<&'t str>: Stream<Token = char>,] {
         internal::select_field_()
     }
 }
 
 mod internal {
-
-    use combine::{ choice, many1, parser::char::char as char_, Parser, attempt};
-
-    use combine::parser::char::letter;
-    use combine::stream::easy;
-    use crate::rib::expr::Expr;
-    use crate::rib::parser::record::record;
-    use combine::parser::char::spaces;
+    use combine::parser::char::char;
+    use crate::rib::parser::rib_expr::rib_expr;
+    use super::*;
 
     pub fn select_field_<'t>() -> impl Parser<easy::Stream<&'t str>, Output = Expr> {
-        (
-            parent_root().skip(spaces()),
-            char_('.').skip(spaces()),
-            field_name().skip(spaces())
-
-        ).map(|(select_field_root, _, field)| {
-            Expr::SelectField(Box::new(select_field_root), field)
-        })
+        (base_expr(), char('.').skip(spaces()), rib_expr())
+            .map(|(base, _, opt)| match opt {
+                Expr::SelectField(expr, field) => match *expr {
+                    Expr::Identifier(first_field) => Expr::SelectField(Box::new(Expr::SelectField(Box::new(base), first_field)), field),
+                    _ => panic!("Failed to parse")
+                }
+                Expr::Identifier(str) => Expr::SelectField(Box::new(base), str),
+                _ => panic!("failed")
+            })
     }
 
-    pub fn field_name<'t>() -> impl Parser<easy::Stream<&'t str>, Output = String> {
-        many1(letter().or(char_('_')))
-            .map(|s: Vec<char>| s.into_iter().collect())
-            .message("Unable to parse field name")
-    }
-
-    pub fn parent_root<'t>() -> impl Parser<easy::Stream<&'t str>, Output = Expr> {
+    fn base_expr<'t>() -> impl Parser<easy::Stream<&'t str>, Output = Expr> {
         choice((
             attempt(record()),
             attempt(field_name().map(Expr::Identifier)),
         ))
     }
 
+    fn field_name<'t>() -> impl Parser<easy::Stream<&'t str>, Output = String> {
+        many1(letter().or(char_('_')))
+            .map(|s: Vec<char>| s.into_iter().collect())
+            .message("Unable to parse field name")
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::rib::parser::rib_expr::rib_expr;
+    use crate::rib::parser::rib_expr::{rib_expr, rib_expr_};
     use combine::EasyParser;
 
     #[test]
     fn test_select_field() {
         let input = "foo.bar";
-        let result = select_field().easy_parse(input);
+        let result = rib_expr().easy_parse(input);
         assert_eq!(
             result,
             Ok((
@@ -72,7 +69,7 @@ mod tests {
     #[test]
     fn test_select_field_from_record() {
         let input = "{foo: bar}.foo";
-        let result = select_field().easy_parse(input);
+        let result = rib_expr().easy_parse(input);
         assert_eq!(
             result,
             Ok((
@@ -88,12 +85,10 @@ mod tests {
         );
     }
 
-    // TODO; Nested selection doesn't work - stack overflow
-    #[ignore]
     #[test]
     fn test_nested_field_selection() {
         let input = "foo.bar.baz";
-        let result = select_field().easy_parse(input);
+        let result = rib_expr().easy_parse(input);
         assert_eq!(
             result,
             Ok((
