ffe1dc27d79cb17483c99de8d122e12322109012
Afsl Thaj <afsal.taj06@gmail.com>
2024-06-26 11:56:49 +1000

    Add license comments back

--- golem-rib/src/parser/literal.rs
+++ golem-rib/src/parser/literal.rs
@@ -1,11 +1,23 @@
+// Copyright 2024 Golem Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use crate::expr::Expr;
 use crate::parser::rib_expr::rib_expr;
 
 use crate::parser::literal::internal::literal_;
 use combine::{easy, parser, Parser, Stream};
 
-// Used parser! even if literal is not recursively used to simplify call sites
-// not dealing with lifetime parameters
 parser! {
     pub fn literal['t]()(easy::Stream<&'t str>) -> Expr
     where [
