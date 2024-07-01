1e598902fb546410a66f17a61f86b04cb31fc78e
Afsal Thaj <afsal.taj06@gmail.com>
2024-07-01 17:26:07 +1000

    Metadata fetcher

--- golem-worker-service-base/src/evaluator/symbol_table.rs
+++ golem-worker-service-base/src/evaluator/symbol_table.rs
@@ -63,7 +63,7 @@ pub mod cached {
     use async_trait::async_trait;
     use golem_common::cache::{BackgroundEvictionMode, Cache, SimpleCache};
     use golem_common::model::ComponentId;
-    use golem_service_base::model::{ComponentMetadata, WorkerId};
+    
     use std::sync::Arc;
 
     // The logic shouldn't be visible outside the crate
