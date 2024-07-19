225964916733684036b34fa177edd6b7fc564e2d
Dávid István Bíró <noise64@gmail.com>
2024-07-19 15:15:47 +0200

    fix cli tests deps

--- golem-cli/tests/main.rs
+++ golem-cli/tests/main.rs
@@ -3,7 +3,9 @@ use std::sync::Arc;
 use libtest_mimic::{Arguments, Conclusion, Failed};
 use tracing::info;
 
-use golem_test_framework::config::{EnvBasedTestDependencies, TestDependencies};
+use golem_test_framework::config::{
+    EnvBasedTestDependencies, EnvBasedTestDependenciesConfig, TestDependencies,
+};
 
 mod api_definition;
 mod api_deployment;
@@ -31,8 +33,13 @@ fn run(deps: Arc<dyn TestDependencies + Send + Sync + 'static>) -> Conclusion {
 fn main() -> Result<(), Failed> {
     env_logger::init();
 
-    let deps: Arc<dyn TestDependencies + Send + Sync + 'static> =
-        Arc::new(EnvBasedTestDependencies::blocking_new_from_worker_executor_cluster_size(3));
+    let deps: Arc<dyn TestDependencies + Send + Sync + 'static> = Arc::new(
+        EnvBasedTestDependencies::blocking_new_from_config(EnvBasedTestDependenciesConfig {
+            worker_executor_cluster_size: 3,
+            keep_docker_containers: false, // will be dropped by testcontainers (current version does not support double rm)
+            ..EnvBasedTestDependenciesConfig::new()
+        }),
+    );
     let cluster = deps.worker_executor_cluster(); // forcing startup by getting it
     info!("Using cluster with {:?} worker executors", cluster.size());
 
