f86deedbd32e9ea3a479e9ebc0bdbaacd91c12b9
Nico Burniske <nicoburniske@gmail.com>
2024-07-31 08:01:38 -0400

    rename rpc module to rust_rpc to avoid naming conflicts

--- golem-worker-executor-base/tests/lib.rs
+++ golem-worker-executor-base/tests/lib.rs
@@ -47,7 +47,7 @@ pub mod guest_languages2;
 pub mod hot_update;
 pub mod keyvalue;
 pub mod measure_test_component_mem;
-pub mod rpc;
+pub mod rust_rpc;
 pub mod scalability;
 pub mod transactions;
 pub mod ts_rpc;
