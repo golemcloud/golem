3c2a1e0cb80af7b645fa739767ac88cf1b22da73
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-08-08 11:19:38 +0200

    Generate bincode codecs for protobuf types

--- build.rs
+++ build.rs
@@ -3,6 +3,10 @@ use std::io::Result;
 fn main() -> Result<()> {
     let mut config = prost_build::Config::new();
     config.type_attribute(".", "#[cfg(feature = \"protobuf\")]");
+    config.type_attribute(
+        ".",
+        "#[cfg_attr(feature=\"bincode\", derive(bincode::Encode, bincode::Decode))]",
+    );
     config.compile_protos(&["proto/wasm/ast/type.proto"], &["proto/"])?;
     Ok(())
 }
