79c0b52eb53a2c320d15f41ac36170647efb23f3
Dávid István Bíró <noise64@gmail.com>
2024-09-15 15:02:07 +0200

    use full version in golem_version

--- golem-common/src/golem_version.rs
+++ golem-common/src/golem_version.rs
@@ -4,7 +4,7 @@ pub use git_version::git_version;
 macro_rules! golem_version {
     () => {
         golem_common::golem_version::git_version!(
-            args = ["--tags", "--abbrev=0"],
+            args = ["--tags", "--long", "--dirty"],
             cargo_prefix = "",
             fallback = "0.0.0"
         )
