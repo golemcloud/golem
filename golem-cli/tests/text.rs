ae18c122433082398a91b3d9302acfa5a4e73c52
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-09-20 15:29:12 +0200

    CLI tests

--- golem-cli/tests/text.rs
+++ golem-cli/tests/text.rs
@@ -2,7 +2,7 @@ use crate::api_definition::{
     golem_def, make_golem_file, make_open_api_file, make_shopping_cart_component,
 };
 use crate::cli::{Cli, CliLive};
-use crate::worker::make_component;
+use crate::worker::add_environment_service_component;
 use assert2::assert;
 use golem_cli::model::component::ComponentView;
 use golem_cli::model::Format;
@@ -291,7 +291,8 @@ fn text_worker_add(
     ),
 ) -> Result<(), Failed> {
     let component_urn =
-        make_component(deps, &format!("{name} text worker add"), &cli)?.component_urn;
+        add_environment_service_component(deps, &format!("{name} text worker add"), &cli)?
+            .component_urn;
     let worker_name = format!("{name}_worker_add");
     let cfg = &cli.config;
     let res = cli.with_format(Format::Text).run_string(&[
@@ -339,8 +340,12 @@ fn text_worker_invoke_and_await(
         CliLive,
     ),
 ) -> Result<(), Failed> {
-    let component_urn =
-        make_component(deps, &format!("{name} text worker_invoke_and_await"), &cli)?.component_urn;
+    let component_urn = add_environment_service_component(
+        deps,
+        &format!("{name} text worker_invoke_and_await"),
+        &cli,
+    )?
+    .component_urn;
     let worker_name = format!("{name}_worker_invoke_and_await");
     let cfg = &cli.config;
     let _: WorkerUrn = cli.run(&[
@@ -384,7 +389,8 @@ fn text_worker_get(
     ),
 ) -> Result<(), Failed> {
     let component_urn =
-        make_component(deps, &format!("{name} text worker get"), &cli)?.component_urn;
+        add_environment_service_component(deps, &format!("{name} text worker get"), &cli)?
+            .component_urn;
     let worker_name = format!("{name}_worker_get");
     let cfg = &cli.config;
     let _: WorkerUrn = cli.run(&[
@@ -451,7 +457,8 @@ fn text_worker_list(
     ),
 ) -> Result<(), Failed> {
     let component_urn =
-        make_component(deps, &format!("{name} text worker list"), &cli)?.component_urn;
+        add_environment_service_component(deps, &format!("{name} text worker list"), &cli)?
+            .component_urn;
     let worker_name = format!("{name:_<9}_worker_list");
     let cfg = &cli.config;
     let _: WorkerUrn = cli.run(&[
