279fa67ae2f1b00b7827e967813583e45fd85fbf
Dávid István Bíró <noise64@gmail.com>
2024-09-24 18:56:44 +0200

    component type checks + naming

--- wasm-rpc-stubgen/src/model/component.rs
+++ wasm-rpc-stubgen/src/model/component.rs
@@ -4,29 +4,51 @@ use std::path::PathBuf;
 
 pub type Result<T> = std::result::Result<T, Vec<String>>;
 
-const TRAIT_TYPE_WORKER_RPC: &str = "worker-rpc";
+pub const TRAIT_TYPE_WASM_RPC: &str = "wasm-rpc";
+
+pub const COMPONENT_TYPE_DURABLE: &str = "durable";
+pub const COMPONENT_TYPE_EPHEMERAL: &str = "ephemeral";
 
 #[derive(Clone, Debug)]
 pub struct Component {
     pub name: String,
-    // TODO: component type
+    pub component_type: ComponentType,
     pub source: PathBuf,
     pub wit: PathBuf,
-    pub wasm: PathBuf,
-    pub composed_wasm: PathBuf,
+    pub input_wasm: PathBuf,
+    pub output_wasm: PathBuf,
     pub worker_rpc_dependencies: Vec<String>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum ComponentType {
+    Durable,
+    Ephemeral,
+}
+
+impl TryFrom<&str> for ComponentType {
+    type Error = String;
+
+    fn try_from(value: &str) -> std::result::Result<Self, Self::Error> {
+        match value {
+            COMPONENT_TYPE_DURABLE => Ok(ComponentType::Durable),
+            COMPONENT_TYPE_EPHEMERAL => Ok(ComponentType::Ephemeral),
+            other => Err(format!("Unknown component type: {}", other)),
+        }
+    }
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ComponentProperties {
     pub wit: String,
-    pub wasm: String,
-    #[serde(rename = "composedWasm")]
-    pub composed_wasm: String,
+    #[serde(rename = "inputWasm")]
+    pub input_wasm: String,
+    #[serde(rename = "outputWasm")]
+    pub output_wasm: String,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct TraitWorkerRpcProperties {
+pub struct TraitWasmRpcProperties {
     #[serde(rename = "componentName")]
     pub component_name: String,
 }
@@ -46,13 +68,30 @@ impl Component {
         if application.application.spec.components.is_empty() {
             add_error("Expected at least one component specification".to_string());
         } else {
-            for component in application.application.spec.components {
+            'component: for component in application.application.spec.components {
                 let mut add_component_error = |err: String| {
                     add_error(format!(
                         "Error in component ({}) specification: {}",
                         component.name, err
                     ));
                 };
+                let log_skip_reason = |reason: String| {
+                    eprintln!(
+                        "{} (component: {}, source: {})",
+                        reason,
+                        component.name,
+                        application.source.to_string_lossy()
+                    );
+                };
+
+                let component_type =
+                    match ComponentType::try_from(component.component_type.as_str()) {
+                        Ok(component_type) => Some(component_type),
+                        Err(err) => {
+                            log_skip_reason(format!("Skipping component: {}", err));
+                            continue 'component;
+                        }
+                    };
 
                 let properties =
                     match serde_json::from_value::<ComponentProperties>(component.properties) {
@@ -71,14 +110,14 @@ impl Component {
                     let mut has_errors = false;
                     for component_trait in component.traits {
                         let properties = match component_trait.trait_type.as_str() {
-                            TRAIT_TYPE_WORKER_RPC => {
-                                match serde_json::from_value::<TraitWorkerRpcProperties>(
+                            TRAIT_TYPE_WASM_RPC => {
+                                match serde_json::from_value::<TraitWasmRpcProperties>(
                                     component_trait.properties,
                                 ) {
                                     Ok(properties) => Some(properties),
                                     Err(err) => {
                                         add_component_error(format!(
-                                            "Failed to get worker RPC trait properties: {}",
+                                            "Failed to get WASM RPC trait properties: {}",
                                             err
                                         ));
                                         has_errors = true;
@@ -87,7 +126,7 @@ impl Component {
                                 }
                             }
                             other => {
-                                eprintln!("Skipping unknown trait: {}", other);
+                                log_skip_reason(format!("Skipping unknown trait: {}", other));
                                 None
                             }
                         };
@@ -96,18 +135,19 @@ impl Component {
                         }
                     }
 
-                    (!has_errors).then(|| worker_rpc_dependencies)
+                    (!has_errors).then_some(worker_rpc_dependencies)
                 };
 
-                if let (Some(properties), Some(worker_rpc_dependencies)) =
-                    (properties, worker_rpc_dependencies)
+                if let (Some(component_type), Some(properties), Some(worker_rpc_dependencies)) =
+                    (component_type, properties, worker_rpc_dependencies)
                 {
                     components.push(Component {
                         name: component.name,
+                        component_type,
                         source: application.source.clone(),
                         wit: properties.wit.into(),
-                        wasm: properties.wasm.into(),
-                        composed_wasm: properties.composed_wasm.into(),
+                        input_wasm: properties.input_wasm.into(),
+                        output_wasm: properties.output_wasm.into(),
                         worker_rpc_dependencies,
                     })
                 }
@@ -159,18 +199,24 @@ kind: Application
 spec:
   components:
     - name: component-one
-      type: component-durable
+      type: durable
       properties:
         wit: wit
-        wasm: out/component.wasm
-        composedWasm: out/component-composed.wasm
+        inputWasm: out/in.wasm
+        outputWasm: out/out.wasm
       traits:
-        - type: worker-rpc
+        - type: wasm-rpc
           properties:
             componentName: component-two
-        - type: worker-rpc
+        - type: wasm-rpc
           properties:
             componentName: component-three
+        - type: unknown-trait
+          properties:
+            unknownProp: test
+    - name: component-one
+      type: unknown-component-type
+      properties:
 "#,
         )
         .unwrap();
@@ -178,22 +224,26 @@ spec:
         assert!(application.api_version == oam::API_VERSION_V1BETA1);
         assert!(application.kind == oam::KIND_APPLICATION);
         assert!(application.metadata.name == "App name");
-        assert!(application.spec.components.len() == 1);
+        assert!(application.spec.components.len() == 2);
 
         let components = Component::from_oam_application(oam::ApplicationWithSource {
             source: PathBuf::from("test"),
             application,
-        })
-        .unwrap();
+        });
+
+        assert!(components.as_ref().err() == None);
+
+        let components = components.unwrap();
 
         assert!(components.len() == 1);
 
         let component = &components[0];
 
         assert!(component.name == "component-one");
+        assert!(component.component_type == ComponentType::Durable);
         assert!(component.wit.to_string_lossy() == "wit");
-        assert!(component.wasm.to_string_lossy() == "out/component.wasm");
-        assert!(component.composed_wasm.to_string_lossy() == "out/component-composed.wasm");
+        assert!(component.input_wasm.to_string_lossy() == "out/in.wasm");
+        assert!(component.output_wasm.to_string_lossy() == "out/out.wasm");
         assert!(component.worker_rpc_dependencies.len() == 2);
 
         assert!(component.worker_rpc_dependencies[0] == "component-two");
