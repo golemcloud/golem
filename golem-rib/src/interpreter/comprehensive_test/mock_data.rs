f06305d674ba6852d439d9a17ef05376c9e2640d
Afsal Thaj <afsal.taj06@gmail.com>
2024-10-01 13:58:41 +1000

    Add comprehensive test module

---
+++ golem-rib/src/interpreter/comprehensive_test/mock_data.rs
@@ -0,0 +1,206 @@
+    use golem_wasm_rpc::protobuf::type_annotated_value::TypeAnnotatedValue;
+    use crate::interpreter::comprehensive_test::{data_types, internal};
+
+    pub(crate) fn result_of_str() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::result_of_str_type(), "ok(\"foo\")")
+    }
+
+    pub(crate) fn result_of_number() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::result_of_number_type(), "ok(42)")
+    }
+
+    pub(crate) fn result_of_option() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::result_of_option_type(), "ok(some(\"foo\"))")
+    }
+
+    pub(crate) fn result_of_variant() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::result_of_variant_type(), "ok(CaseStr(\"foo\"))")
+    }
+
+    pub(crate) fn result_of_enum() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::result_of_enum_type(), "ok(EnumA)")
+    }
+
+    // TBD
+    pub(crate) fn result_of_tuple() -> TypeAnnotatedValue {
+        let tuple_str = internal::convert_type_annotated_value_to_str(&tuple());
+        let wave_str = format!("ok({})", tuple_str);
+        internal::get_type_annotated_value(&data_types::result_of_tuple_type(), wave_str.as_str())
+    }
+
+    pub(crate) fn result_of_flag() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::result_of_flag_type(), "ok({FeatureX})")
+    }
+
+    pub(crate) fn result_of_record() -> TypeAnnotatedValue {
+        let record_str = internal::convert_type_annotated_value_to_str(&record());
+        let wave_str = format!("ok({})", &record_str);
+        internal::get_type_annotated_value(&data_types::result_of_record_type(), wave_str.as_str())
+
+    }
+
+    pub(crate) fn result_of_list() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::result_of_list_type(), "ok([\"foo\"])")
+    }
+
+    pub(crate) fn list_of_number() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::list_of_number_type_type(), "[42]")
+    }
+
+    pub(crate) fn list_of_str() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::list_of_str_type(), "[\"foo\"]")
+    }
+
+    pub(crate) fn list_of_option() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::list_of_option_type(), "[Some(\"foo\")]")
+    }
+
+    pub(crate) fn list_of_list() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::list_of_list_type(), "[[\"foo\"]]")
+    }
+
+    pub(crate) fn list_of_variant() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::list_of_variant_type(), "[CaseStr(\"foo\")]")
+    }
+
+    pub(crate) fn list_of_enum() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::list_of_enum_type(), "[EnumA]")
+    }
+
+    pub(crate) fn list_of_tuple() -> TypeAnnotatedValue {
+        let tuple_str = internal::convert_type_annotated_value_to_str(&tuple());
+        let wave_str = format!("[{}, {}]", &tuple_str, &tuple_str);
+        internal::get_type_annotated_value(&data_types::list_of_tuple(), wave_str.as_str())
+    }
+
+    pub(crate) fn list_of_record() -> TypeAnnotatedValue {
+        let record_str = internal::convert_type_annotated_value_to_str(&record());
+        let wave_str = format!("[{}]", &record_str);
+        internal::get_type_annotated_value(&data_types::list_of_record_type(), wave_str.as_str())
+    }
+
+    pub(crate) fn option_of_number() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::option_of_number_type(), "Some(42)")
+    }
+
+    pub(crate) fn option_of_str() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::option_of_str_type(), "Some(\"foo\")")
+    }
+
+    pub(crate) fn option_of_option() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::option_of_option_type(), "Some(Some(\"foo\"))")
+    }
+
+    pub(crate) fn option_of_variant() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::option_of_variant_type(), "Some(CaseStr(\"foo\"))")
+    }
+
+    pub(crate) fn option_of_enum() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::option_of_enum_type(), "Some(EnumA)")
+    }
+
+    pub(crate) fn option_of_tuple() -> TypeAnnotatedValue {
+        let tuple_str = internal::convert_type_annotated_value_to_str(&tuple());
+        let wave_str = format!("Some({})", tuple_str);
+        internal::get_type_annotated_value(&data_types::option_of_tuple(), wave_str.as_str())
+    }
+
+    pub(crate) fn option_of_record() -> TypeAnnotatedValue {
+        let record_str = internal::convert_type_annotated_value_to_str(&record());
+        let wave_str = format!("some({})", &record_str);
+        internal::get_type_annotated_value(&data_types::list_of_record_type(), wave_str.as_str())
+    }
+
+    pub(crate) fn option_of_list() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::option_of_list(), "some([\"foo\"])")
+    }
+
+    pub(crate) fn tuple() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::tuple_type(), r#"
+          (\"foo\", 42, 42, 42, 42, true, 'a', some(42), ok(42), [true], CaseF64(42.0), {\"field_one\": true, \"field_two\": \"foo\"})"#
+        )
+    }
+
+    pub(crate) fn enum_data() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::enum_type(), "EnumA")
+    }
+
+    pub(crate) fn str_data() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::str_type(), "foo")
+    }
+
+    pub(crate) fn number_data() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::number_type(), "42")
+    }
+
+    pub(crate) fn flag() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::flag_type(), "{FeatureX}")
+    }
+
+    pub(crate) fn variant() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::variant_type(), "CaseStr(\"foo\")")
+    }
+
+    pub(crate) fn record() -> TypeAnnotatedValue {
+        internal::get_type_annotated_value(&data_types::record_type(), r#"
+          {
+            string_headers: {authorization_string: "foo"},
+            data_body: {
+              str: "foo",
+              list_of_str: ["foo"],
+              list_of_option: ["foo"],
+              list_of_list: [["foo"]],
+              list_of_variant: ["CaseStr(\"foo\")"],
+              list_of_enum: ["EnumA"],
+              list_of_tuple: [("foo", 42)],
+              list_of_record: [{"field_string_one": "foo", "field_string_two": "foo"}],
+              option_of_str: some("foo"),
+              option_of_option: some(some("foo")),
+              option_of_variant: some(CaseStr(\"foo\")),
+              option_of_enum: some("EnumA"),
+              option_of_tuple: some(("foo", 42)),
+              option_of_record: some({"field_string_one": "foo", "field_string_two": "foo"}),
+              option_of_list: some(["foo"]),
+              nested_record: {"field_string_one": "foo", "field_string_two": "foo"},
+              variant_data_1: CaseStr(\"foo\"),
+              variant_data_2: CaseStr(\"foo\"),
+              variant_data_3: CaseStr(\"foo\"),
+              variant_data_4: CaseStr(\"foo\"),
+              variant_data_5: CaseStr(\"foo\"),
+              variant_data_6: CaseStr(\"foo\"),
+              enum_data_1: EnumA,
+              enum_data_2: EnumB,
+              enum_data_3: EnumC,
+              flags_data_1: { FeatureX },
+              flags_data_2: { FeatureX, FeatureY },
+              flags_data_3: { FeatureX, FeatureY, FeatureZ },
+              result_data_1: ok("foo"),
+              result_data_2: ok(42),
+              result_data_3: ok(EnumA),
+              result_data_4: ok(CaseStr(\"foo\")),
+              result_data_5: ok(("foo", 42)),
+              result_data_6: ok(Some(\"foo\")),
+              result_data_7: err("foo"),
+              result_data_8: err(42),
+              result_data_9: err(EnumA),
+              result_data_10: err(CaseStr(\"foo\")),
+              result_data_11: err(("foo", 42)),
+              result_data_12: err(Some(\"foo\")),
+              result_data_13: ok({"field_string_one": "foo", "field_string_two": "foo"}),
+              result_data_14: err({"field_string_one": "foo", "field_string_two": "foo"}),
+              result_data_15: ok({ FeatureX, FeatureY, FeatureZ }),
+              result_data_16: err({ FeatureX, FeatureY, FeatureZ }),
+              character_data : 'x',
+              f64_data : 3.14,
+              f32_data : 3.14,
+              u64_data : 42,
+              s64_data : 42,
+              u32_data : 42,
+              s32_data : 42,
+              u16_data : 42,
+              s16_data : 42,
+              u8_data : 42,
+              s8_data : 42,
+              boolean_data : true,
+           }"#)
+    }
---
+++ golem-rib/src/interpreter/comprehensive_test/mock_interpreter.rs
@@ -0,0 +1,132 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use golem_wasm_ast::analysis::{AnalysedType, TypeStr};
+use golem_wasm_rpc::protobuf::type_annotated_value::TypeAnnotatedValue;
+use golem_wasm_rpc::protobuf::TypedTuple;
+use crate::{Interpreter, RibFunctionInvoke};
+use crate::interpreter::comprehensive_test::{data, test_utils};
+use crate::interpreter::env::InterpreterEnv;
+use crate::interpreter::stack::InterpreterStack;
+
+
+pub(crate) fn interpreter() -> Interpreter {
+    let record_input_type = test_utils::analysed_type_record(vec![
+        (
+            "headers",
+            test_utils::analysed_type_record(vec![("name", AnalysedType::Str(TypeStr))]),
+        ),
+        (
+            "body",
+            test_utils::analysed_type_record(vec![
+                ("name", AnalysedType::Str(TypeStr)),
+            ]),
+        ),
+        (
+            "path",
+            test_utils::analysed_type_record(vec![("name", AnalysedType::Str(TypeStr))]),
+        ),
+    ]);
+
+
+    let functions_and_results: Vec<(&str, Option<TypeAnnotatedValue>)> =
+        vec![
+            ("function-unit-response", None),
+            ("function-no-arg", Some(data::str_data())),
+            ("function-no-arg-unit", None),
+            ("function-str-response", Some(data::str_data())),
+            ("function-number-response", Some(data::number_data())),
+            ("function-option-str-response", Some(data::option_of_str())),
+            ("function-option-number-response", Some(data::option_of_number())),
+            ("function-option-option-response", Some(data::option_of_option())),
+            ("function-option-variant-response", Some(data::option_of_variant())),
+            ("function-option-enum-response", Some(data::option_of_enum())),
+            ("function-option-tuple-response", Some(data::option_of_tuple())),
+            ("function-option-record-response", Some(data::option_of_record())),
+            ("function-option-list-response", Some(data::option_of_list())),
+            ("function-list-number-response", Some(data::list_of_number())),
+            ("function-list-str-response", Some(data::list_of_str())),
+            ("function-list-option-response", Some(data::list_of_option())),
+            ("function-list-list-response", Some(data::list_of_list())),
+            ("function-list-variant-response", Some(data::list_of_variant())),
+            ("function-list-enum-response", Some(data::list_of_enum())),
+            ("function-list-tuple-response", Some(data::list_of_tuple())),
+            ("function-list-record-response", Some(data::list_of_record())),
+            ("function-result-str-response", Some(data::result_of_str())),
+            ("function-result-number-response", Some(data::result_of_number())),
+            ("function-result-option-response", Some(data::result_of_option())),
+            ("function-result-variant-response", Some(data::result_of_variant())),
+            ("function-result-enum-response", Some(data::result_of_enum())),
+            ("function-result-tuple-response", Some(data::result_of_tuple())),
+            ("function-result-flag-response", Some(data::result_of_flag())),
+            ("function-result-record-response", Some(data::result_of_record())),
+            ("function-result-list-response", Some(data::result_of_list())),
+            ("function-tuple-response", Some(data::tuple())),
+            ("function-enum-response", Some(data::enum_data())),
+            ("function-flag-response", Some(data::flag())),
+            ("function-variant-response", Some(data::variant())),
+            ("function-record-response", Some(data::record())),
+            ("function-all-inputs", Some(data::str_data()))
+        ];
+
+    let functions_and_result: HashMap<FunctionName, Option<TypeAnnotatedValue>> = functions_and_results
+        .into_iter()
+        .map(|(name, result)| (FunctionName(name.to_string()), result))
+        .collect();
+
+    let interpreter_env_input: HashMap<String, TypeAnnotatedValue> = HashMap::new();
+
+    dynamic_test_interpreter(functions_and_result, interpreter_env_input)
+}
+
+
+#[derive(Clone, Hash, PartialEq, Eq)]
+struct FunctionName(pub(crate) String);
+
+
+fn dynamic_test_interpreter(
+    functions_and_result: HashMap<FunctionName, Option<TypeAnnotatedValue>>,
+    interpreter_env_input: HashMap<String, TypeAnnotatedValue>,
+) -> Interpreter {
+    Interpreter {
+        stack: InterpreterStack::default(),
+        env: InterpreterEnv::from(
+            interpreter_env_input,
+            dynamic_worker_invoke(functions_and_result),
+        ),
+    }
+}
+
+fn dynamic_worker_invoke(
+    functions_and_result: HashMap<FunctionName, Option<TypeAnnotatedValue>>,
+) -> RibFunctionInvoke {
+    let value = functions_and_result.clone();
+
+    Arc::new(move |a, _| {
+        Box::pin({
+            let value = value.get(&FunctionName(a)).cloned().flatten();
+            let analysed_type = value.clone().map(|x| AnalysedType::try_from(&x).unwrap());
+
+            async move {
+                let analysed_type = analysed_type.clone();
+                let value = value.clone();
+
+                if let Some(value) = value {
+                    Ok(TypeAnnotatedValue::Tuple(TypedTuple {
+                        typ: vec![golem_wasm_ast::analysis::protobuf::Type::from(
+                            &analysed_type.unwrap(),
+                        )],
+                        value: vec![golem_wasm_rpc::protobuf::TypeAnnotatedValue {
+                            type_annotated_value: Some(value),
+                        }],
+                    }))
+                } else {
+                    // Representing Unit
+                    Ok(TypeAnnotatedValue::Tuple(TypedTuple {
+                        typ: vec![],
+                        value: vec![],
+                    }))
+                }
+            }
+        })
+    })
+}
