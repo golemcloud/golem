b369b184e68c6b880497217dc6ef6e182a0056f3
Afsal Thaj <afsal.taj06@gmail.com>
2024-10-06 16:41:43 +1100

    Fix test case

--- golem-rib/src/type_inference/mod.rs
+++ golem-rib/src/type_inference/mod.rs
@@ -1304,7 +1304,7 @@ mod type_inference_tests {
         fn test_pattern_match_with_record_with_select_field() {
             let expr_str = r#"
               let x = { foo : "bar" };
-              match 1 {
+              match some(x) {
                 some(x) => x.foo
               }
             "#;
