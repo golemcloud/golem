ae6a47971411def69bed5b82b0c7ef373c5c4b20
Dávid István Bíró <noise64@gmail.com>
2024-10-07 10:22:01 +0200

    extract stubgen tests that use cargo to tests-integration, and run them sequentially to avoid parallel cargo build problems

--- wasm-rpc-stubgen/tests-integration/tests/compose.rs
+++ wasm-rpc-stubgen/tests-integration/tests/compose.rs
@@ -23,9 +23,9 @@ use golem_wasm_rpc_stubgen::commands::composition::compose;
 use golem_wasm_rpc_stubgen::commands::dependencies::{add_stub_dependency, UpdateCargoToml};
 use golem_wasm_rpc_stubgen::commands::generate::generate_and_build_stub;
 use golem_wasm_rpc_stubgen::stub::StubDefinition;
-use golem_wasm_rpc_stubgen::WasmRpcOverride;
 use std::path::{Path, PathBuf};
 use tempfile::TempDir;
+use wasm_rpc_stubgen_tests_integration::{test_data_path, wasm_rpc_override};
 
 #[tokio::test]
 async fn compose_with_single_stub() {
@@ -101,7 +101,7 @@ async fn compose_with_single_stub_not_importing_stub() {
 async fn init_stub(name: &str) -> (TempDir, PathBuf) {
     let tempdir = TempDir::new().unwrap();
 
-    let source_wit_root = Path::new("test-data").join(name);
+    let source_wit_root = test_data_path().join(name);
     let canonical_target_root = tempdir.path().canonicalize().unwrap();
 
     let def = StubDefinition::new(
@@ -109,18 +109,7 @@ async fn init_stub(name: &str) -> (TempDir, PathBuf) {
         &canonical_target_root,
         &None,
         "1.0.0",
-        &WasmRpcOverride {
-            wasm_rpc_version_override: None,
-            wasm_rpc_path_override: Some(
-                std::env::current_dir()
-                    .unwrap()
-                    .parent()
-                    .unwrap()
-                    .join("wasm-rpc")
-                    .to_string_lossy()
-                    .to_string(),
-            ),
-        },
+        &wasm_rpc_override(),
         false,
     )
     .unwrap();
@@ -129,17 +118,17 @@ async fn init_stub(name: &str) -> (TempDir, PathBuf) {
 }
 
 fn init_caller(name: &str) -> TempDir {
-    let tempdir = TempDir::new().unwrap();
-    let source = Path::new("test-data").join(name);
+    let temp_dir = TempDir::new().unwrap();
+    let source = test_data_path().join(name);
 
     fs_extra::dir::copy(
         source,
-        tempdir.path(),
+        temp_dir.path(),
         &CopyOptions::new().content_only(true).overwrite(true),
     )
     .unwrap();
 
-    tempdir
+    temp_dir
 }
 
 fn compile_rust(path: &Path) {
