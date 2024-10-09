3efe3ce8c919d810b57a0d1e10eb93b6ea13ee44
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-10-09 21:01:35 +0200

    Switch to test-r in main too

--- cli/wasm-rpc-stubgen/src/main.rs
+++ cli/wasm-rpc-stubgen/src/main.rs
@@ -17,6 +17,9 @@ use colored::Colorize;
 use golem_wasm_rpc_stubgen::*;
 use std::process::ExitCode;
 
+#[cfg(test)]
+test_r::enable!();
+
 #[tokio::main]
 async fn main() -> ExitCode {
     pretty_env_logger::init();
