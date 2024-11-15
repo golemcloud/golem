ba7eb81ae14947a370707c03c94283061ce661d8
Afsal Thaj <afsal.taj06@gmail.com>
2024-11-15 12:07:26 +1100

    Reformat code

--- golem-worker-service-base/src/gateway_security/security_scheme_metadata.rs
+++ golem-worker-service-base/src/gateway_security/security_scheme_metadata.rs
@@ -1,5 +1,8 @@
 use crate::gateway_security::{GolemIdentityProviderMetadata, SecurityScheme};
 
+// This can exist as part of the middleware to initiate the authorisation workflow
+// redirecting user to provider login page, or it can be part of the static binding
+// serving the auth_call_back endpoint that's called by the provider after the user logs in.
 #[derive(Debug, Clone, PartialEq)]
 pub struct SecuritySchemeWithProviderMetadata {
     pub security_scheme: SecurityScheme,
