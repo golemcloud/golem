0f3c92d517c00231151519e58816a488e0de0828
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-12-05 06:49:44 +0100

    Fix build script for windows (#1134)

--- golem-worker-executor-base/build.rs
+++ golem-worker-executor-base/build.rs
@@ -51,9 +51,14 @@ fn find_package_root(name: &str) -> String {
 }
 
 fn preview2_mod_gen(golem_wit_path: &str) -> String {
+    let golem_wit_path = Path::new(golem_wit_path)
+        .join("wit")
+        .to_str()
+        .map(|s| s.to_string())
+        .unwrap();
     format!(
         r#"wasmtime::component::bindgen!({{
-        path: "{golem_wit_path}/wit",
+        path: r"{golem_wit_path}",
         interfaces: "
           import golem:api/host@0.2.0;
           import golem:api/host@1.1.0;
