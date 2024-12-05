9e015dc016fe6473facc644a1a7e51dd03eba9e0
Afsal Thaj <afsal.taj06@gmail.com>
2024-12-05 17:51:54 +1100

    Fix schema

--- golem-component-service/db/migration/postgres/008__add_transformed_object_store_key.sql
+++ golem-component-service/db/migration/postgres/008__add_transformed_object_store_key.sql
@@ -1,2 +1,2 @@
 ALTER TABLE component_versions
-    ADD COLUMN IF NOT EXISTS transformed_object_store_key text NOT NULL default 'default_value';;
+    ADD COLUMN IF NOT EXISTS transformed_object_store_key text NOT NULL default 'default_value';
