914e351bdaae730d9a56e0e7ce850bfa68743ecf
Johnpaul <polymenjohn1@gmail.com>
2024-12-15 20:14:07 +0100

    setup ts files

---
+++ golem-ui-service/frontend/src/App.tsx
@@ -0,0 +1,49 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import {
+  ComponentDetail
+} from './pages/ComponentDetail';
+import { Components } from './pages/Components';
+import { Layout } from './components/layout/Layout';
+import { Overview } from './pages/Overview';
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Overview />} />
+          <Route path="/workers" element={<div>Workers Page</div>} />
+          <Route path="/components" element={<Components />} />
+          <Route path="/components/:id" element={<ComponentDetail />} />
+          <Route path="/plugins" element={<div>Plugins Page</div>} />
+          <Route path="/api" element={<div>API Page</div>} />
+        </Routes>
+        <div>
+          {/* This component will render the toasts */}
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              duration: 5000,
+              style: {
+                background: '#1F2937',
+                color: '#F3F4F6',
+                border: '1px solid #374151',
+              },
+              success: {
+                icon: '✅',
+              },
+              error: {
+                icon: '❌',
+                duration: 7000,
+              },
+            }}
+          />
+        </div>
+      </Layout>
+    </BrowserRouter>
+  );
+}
+
+export default App;
