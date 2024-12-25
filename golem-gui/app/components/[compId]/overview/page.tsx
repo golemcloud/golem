8c7d26a77f9ad1c11d42303a9ae2889c8fe7daf3
Mubashirshariq <mubashirsharik01@gmail.com>
2024-12-25 17:11:48 +0530

    minor change

--- golem-gui/app/components/[compId]/overview/page.tsx
+++ golem-gui/app/components/[compId]/overview/page.tsx
@@ -25,56 +25,67 @@ import CustomModal from "@/components/CustomModal";
 import useComponents from "@/lib/hooks/use-component";
 import { useParams } from "next/navigation";
 import { ComponentExport, WorkerFunction } from "@/types/api";
-import useWorkers, {getStateFromWorkersData} from "@/lib/hooks/use-worker";
+import useWorkers, { getStateFromWorkersData } from "@/lib/hooks/use-worker";
 
 const Overview = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { compId } = useParams<{ compId: string }>();
 
-  const { components, isLoading: componentDataLoading } = useComponents(compId, "latest");
+  const { components, isLoading: componentDataLoading } = useComponents(
+    compId,
+    "latest"
+  );
   const [latestComponent] = components;
 
-  const {workers, isLoading} = useWorkers(compId);
+  const { workers, isLoading } = useWorkers(compId);
 
-  const workerStats = useMemo(()=>{
-      const stats = getStateFromWorkersData(workers);
-      return       [{
+  const workerStats = useMemo(() => {
+    const stats = getStateFromWorkersData(workers);
+    return [
+      {
         label: "Active Workers",
-        value: Object.keys(stats)?.reduce((acc, key)=> acc+=(key!="failed"? stats[key]:0), 0) || 0,
+        value:
+          Object.keys(stats)?.reduce(
+            (acc, key) => (acc += key != "failed" ? stats[key] : 0),
+            0
+          ) || 0,
         icon: <CheckCircleOutline fontSize="large" />,
-        isLoading: isLoading
+        isLoading: isLoading,
       },
       {
         label: "Running Workers",
         value: stats["running"] || 0,
         icon: <RocketLaunch fontSize="large" />,
-        isLoading: isLoading
+        isLoading: isLoading,
       },
       {
         label: "Failed Workers",
         value: stats["failed"] || 0,
         icon: <ErrorOutline fontSize="large" />,
-        isLoading: isLoading
-      },]
+        isLoading: isLoading,
+      },
+    ];
+  }, [workers]);
 
-  }, [workers])
- 
   const stats = useMemo(() => {
     return [
       {
         label: "Latest Component Version",
         value: latestComponent?.versionedComponentId?.version,
         icon: <InsertChart fontSize="large" />,
-        isLoading: componentDataLoading
+        isLoading: componentDataLoading,
       },
-      
     ];
   }, [latestComponent]);
 
   const exports = useMemo(() => {
-    const metaExports = (latestComponent?.metadata?.exports || []) as ComponentExport[];
-    return metaExports.flatMap((expo: ComponentExport) =>
-      expo?.functions?.map((fun: WorkerFunction) => `${expo.name}.${fun.name}`) || []
+    const metaExports = (latestComponent?.metadata?.exports ||
+      []) as ComponentExport[];
+    return metaExports.flatMap(
+      (expo: ComponentExport) =>
+        expo?.functions?.map(
+          (fun: WorkerFunction) => `${expo.name}.${fun.name}`
+        ) || []
     );
   }, [latestComponent?.versionedComponentId?.version]);
   // const handleOpen = () => setIsOpen(true);
@@ -103,12 +114,18 @@ const Overview = () => {
         {/* Stats Section */}
         {[...stats, ...workerStats].map((stat, index) => (
           <Grid item xs={12} sm={6} md={3} key={index}>
-            <Paper sx={{ padding: 3, textAlign: "center", bgcolor: "#1E1E1E" }}>
-              {stat.icon}
-              <Typography variant="h5" sx={{ marginTop: 1 }}>
+            <Paper sx={{ padding: 2, textAlign: "center", bgcolor: "#1E1E1E" }}>
+              <Box sx={{display:"flex", justifyContent:"space-between"}}>
+                <Typography variant="body2">{stat.label}</Typography>
+                 <Typography sx={{fontSize:"4px"}}>
+                  {stat.icon}
+                 </Typography>
+                
+              </Box>
+
+              <Typography  variant="h5" sx={{ marginTop: 1,display:"flex" }}>
                 {stat?.isLoading ? "Loading..." : stat.value}
               </Typography>
-              <Typography variant="body1">{stat.label}</Typography>
             </Paper>
           </Grid>
         ))}
@@ -128,54 +145,71 @@ const Overview = () => {
           </Paper>
         </Grid>
 
-           {/* Worker Status */}
-              <Grid item xs={12} md={6}>
-                <Paper    sx={{
+        {/* Worker Status */}
+        <Grid item xs={12} md={6}>
+          <Paper
+            sx={{
               padding: 4,
               bgcolor: "#1E1E1E",
               height: 550,
             }}
->
-          <Typography variant="h6" sx={{ marginBottom: 2 }}>
-            Worker Status
-          </Typography>
-          <Divider sx={{ bgcolor: "#424242", marginY: 1 }} />
-          {workerStats.reduce((acc, stat) => acc + stat.value, 0) > 0 ? (
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'relative', height: 200,marginTop:15 }}>
-              <CircularProgress
-                variant="determinate"
-                value={((workerStats[0]?.value || 0) / workerStats.reduce((acc, stat) => acc + stat.value, 0)) * 100}
-                size={300}
-                thickness={10}
-                sx={{
-                  color: '#36b4a5',
-                  position: 'absolute',
-                }}
-              />
-              {/* Center Text */}
+          >
+            <Typography variant="h6" sx={{ marginBottom: 2 }}>
+              Worker Status
+            </Typography>
+            <Divider sx={{ bgcolor: "#424242", marginY: 1 }} />
+            {workerStats.reduce((acc, stat) => acc + stat.value, 0) > 0 ? (
               <Box
                 sx={{
-                  position: 'absolute',
-                  textAlign: 'center',
-                  color: '#fff',
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  position: "relative",
+                  height: 200,
+                  marginTop: 15,
                 }}
               >
-                <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
-                  {workerStats.reduce((acc, stat) => acc + stat.value, 0)}
-                </Typography>
-                <Typography variant="body2" sx={{ color: '#ccc' }}>
-                  Total Workers
-                </Typography>
+                <CircularProgress
+                  variant="determinate"
+                  value={
+                    ((workerStats[0]?.value || 0) /
+                      workerStats.reduce((acc, stat) => acc + stat.value, 0)) *
+                    100
+                  }
+                  size={300}
+                  thickness={10}
+                  sx={{
+                    color: "#36b4a5",
+                    position: "absolute",
+                  }}
+                />
+                {/* Center Text */}
+                <Box
+                  sx={{
+                    position: "absolute",
+                    textAlign: "center",
+                    color: "#fff",
+                  }}
+                >
+                  <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+                    {workerStats.reduce((acc, stat) => acc + stat.value, 0)}
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: "#ccc" }}>
+                    Total Workers
+                  </Typography>
+                </Box>
               </Box>
-            </Box>
-          ) : (
-            <Typography>No workers found</Typography>
-          )}
-        </Paper>
+            ) : (
+              <Typography>No workers found</Typography>
+            )}
+          </Paper>
+        </Grid>
       </Grid>
-    </Grid>
       <CustomModal open={isOpen} onClose={handleClose} heading="Create Worker">
-        <CreateWorker compId={compId} version={latestComponent?.versionedComponentId?.version}/>
+        <CreateWorker
+          compId={compId}
+          version={latestComponent?.versionedComponentId?.version}
+        />
       </CustomModal>
     </Box>
   );
