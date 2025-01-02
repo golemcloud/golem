a8996aab0cb88cc8496c45435e47ab0824594a35
itsparser <itsparser@gmail.com>
2025-01-02 20:19:50 +0530

    Added updated for component

--- crates/webapp/src/pages/components/details/update.tsx
+++ crates/webapp/src/pages/components/details/update.tsx
@@ -1,38 +1,115 @@
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 
 import ComponentLeftNav from "./componentsLeftNav";
-import WasmUpload from "../create/wasmUpload.tsx";
-import FileManager from "../create/fileManager.tsx";
+import {Card, CardContent, CardDescription, CardTitle} from "@/components/ui/card.tsx";
+import {Form, FormControl, FormField, FormItem, FormLabel} from "@/components/ui/form.tsx";
+import {Input} from "@/components/ui/input.tsx";
+import {FileUp} from "lucide-react";
+import {Button} from "@/components/ui/button.tsx";
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {API} from "@/service";
+import {useRef, useState} from "react";
+import {toast} from "@/hooks/use-toast.ts";
+import ErrorBoundary from "@/components/errorBoundary.tsx";
+
+const formSchema = z.object({
+    component: z.instanceof(File).refine((file) => file.size < 50000000, {
+        message: 'Your resume must be less than 50MB.',
+    }),
+})
 
 export default function ComponentUpdate() {
-  const { componentId } = useParams();
+    const {componentId} = useParams();
+    const [file, setFile] = useState<File | null>(null);
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            component: undefined,
+        },
+    });
 
-  return (
-    <div className="flex">
-      <ComponentLeftNav />
-      <div className="flex-1 p-8">
-        <div className="flex items-center justify-between mb-8">
-          <div className="grid grid-cols-2 gap-4">
-            <h1 className="text-2xl font-semibold mb-2">{componentId}</h1>
-            <div className="flex items-center gap-2">
-              <span className="inline-flex items-center rounded-md px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 bg-primary-background text-primary-soft hover:bg-primary/50 active:bg-primary/50 border border-primary-border w-fit font-mono">
+    function onSubmit() {
+        const formData = new FormData();
+        formData.append("component", file!);
+        API.updateComponent(componentId!, formData).then((res) => {
+            console.log("res ", res);
+            form.reset();
+            setFile(null);
+            toast({
+                title: "New Component Added",
+                description: "New Component Added",
+            });
+        });
+    }
+
+    return (
+        <ErrorBoundary>
+            <div className="flex">
+                <ComponentLeftNav/>
+                <div className="flex-1 p-8">
+                    <div className="flex items-center justify-between mb-8">
+                        <div className="grid grid-cols-2 gap-4">
+                            <h1 className="text-2xl font-semibold mb-2">{componentId}</h1>
+                            <div className="flex items-center gap-2">
+              <span
+                  className="inline-flex items-center rounded-md px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 bg-primary-background text-primary-soft hover:bg-primary/50 active:bg-primary/50 border border-primary-border w-fit font-mono">
                 0.1.0
               </span>
+                            </div>
+                        </div>
+                    </div>
+                    <Card className="max-w-4xl mx-auto border-0 shadow-none" key={"component.componentName"}>
+                        <CardTitle><h1 className="text-2xl font-semibold mb-1">Create a new Component</h1></CardTitle>
+                        <CardDescription><p className="text-sm text-gray-400">Components are the building blocks</p>
+                        </CardDescription>
+                        <CardContent className="p-6">
+                            <Form {...form}>
+                                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                                    <FormField
+                                        control={form.control}
+                                        name="component"
+                                        render={({field: {value, onChange, ...fieldProps}}) => (
+                                            <FormItem>
+                                                <FormLabel>Component</FormLabel>
+                                                <FormControl>
+                                                    <div
+                                                        className="border-2 border-dashed border-gray-200 rounded-lg p-8 cursor-pointer hover:border-gray-400"
+                                                        onClick={() => fileInputRef?.current?.click()}
+                                                    >
+                                                        <div
+                                                            className="flex flex-col items-center justify-center text-center">
+                                                            <FileUp className="h-8 w-8 text-gray-400 mb-3"/>
+                                                            <Input type="file" accept="application/wasm,.wasm"
+                                                                   className="hidden" {...fieldProps}
+                                                                   ref={fileInputRef}
+                                                                   onChange={(event) => {
+                                                                       setFile(event.target.files && event.target.files[0]);
+                                                                       return onChange(event.target.files && event.target.files[0]);
+                                                                   }}/>
+                                                            <p className="text-sm text-gray-500 mb-4">File up to
+                                                                50MB</p>
+                                                            <p className="font-medium mb-1">
+                                                                {file ? file.name : "Upload Component WASM"}
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </FormControl>
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <div className="flex justify-end">
+                                        <Button type="submit">Update Component</Button>
+                                    </div>
+                                </form>
+                            </Form>
+                        </CardContent>
+                    </Card>
+
+                </div>
             </div>
-          </div>
-        </div>
-        <div className="max-w-4xl mx-auto p-6">
-          <h1 className="text-3xl font-semibold mb-2">Update Component</h1>
-          <p className="text-gray-500 text-lg mb-8">Update component version</p>
-          <WasmUpload />
-          <FileManager />
-          <div className="flex justify-end">
-            <button className="flex items-center space-x-2 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
-              <span>Update</span>
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+        </ErrorBoundary>
+    );
 }
