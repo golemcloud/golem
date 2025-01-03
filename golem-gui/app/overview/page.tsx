be00089c1c28c2cebcc06cd5fecced0f7a12a418
Rajesh Jonnalagadda <rajeshjonnalagadda48@gmail.com>
2025-01-03 17:01:50 +0530

    chore: handle resources links and navigations

--- golem-gui/app/overview/page.tsx
+++ golem-gui/app/overview/page.tsx
@@ -32,21 +32,28 @@ const ProjectDashboard = () => {
       label: "Language Guides",
       icon: <NotepadText />,
       description: "Check our language and start building",
+      link:"https://learn.golem.cloud/docs/develop-overview"
     },
     {
       label: "Components",
       icon: <Component />,
       description: "Create Wasm components that run on Golem",
+      link:"https://learn.golem.cloud/docs/concepts/components"
+
     },
     {
       label: "APIs",
       icon: <Globe />,
       description: "Craft custom APIs to expose your components to the world",
+      link:"https://learn.golem.cloud/docs/rest-api/oss-rest-api"
+
     },
     {
       label: "Workers",
       icon: <Bot />,
       description: "Launch and manage efficient workers from your components",
+      link:"https://learn.golem.cloud/docs/concepts/workers"
+
     },
   ];
   const [open, setOpen] = useState<string | null>(null);
--- golem-gui/components/ui/footer-links.tsx
+++ golem-gui/components/ui/footer-links.tsx
@@ -1,39 +1,37 @@
-"use client"
+"use client";
 
 import React from "react";
-import {
-  Box,
-  Typography,
-  Card,
-  Grid2 as MuiGrid,
-
-} from "@mui/material";
+import { Box, Typography, Card, Grid2 as MuiGrid } from "@mui/material";
+import Link from "next/link";
 
 interface Resource {
-    label: string;
-    icon: JSX.Element;
-    description: string;
-} 
+  label: string;
+  icon: JSX.Element;
+  description: string;
+  link?: string;
+}
 interface ResourcesProps {
-    resources: Resource[];
-    variant: "main"|"others";
+  resources: Resource[];
+  variant: "main" | "others";
 }
 
-export default function FooterLinks({resources,variant}: ResourcesProps) {
-    
-
-    return (
-        <Box>
-        {variant === "main" &&
+export default function FooterLinks({ resources, variant }: ResourcesProps) {
+  return (
+    <Box>
+      {variant === "main" && (
         <Typography variant="h5" sx={{ mb: 3, fontWeight: 600 }}>
           Resources
         </Typography>
-}
-        <MuiGrid container spacing={1}>
-          {resources.map((resource:Resource) => (
-            <MuiGrid
-              size={{ xs: 12, sm: 6, md: 6, lg: 3 }}
-              key={resource.label}
+      )}
+      <MuiGrid container spacing={1}>
+        {resources.map((resource: Resource) => (
+          <MuiGrid size={{ xs: 12, sm: 6, md: 6, lg: 3 }} key={resource.label}>
+            <Link
+              href={resource.link || "#"}
+              style={{ textDecoration: "none", color: "inherit" }}
+              passHref
+              target="_blank" // Opens the link in a new tab
+              rel="noopener noreferrer" // Improves security by preventing access to the window.opener object
             >
               <Card
                 sx={{
@@ -69,9 +67,10 @@ export default function FooterLinks({resources,variant}: ResourcesProps) {
                   {resource.description}
                 </Typography>
               </Card>
-            </MuiGrid>
-          ))}
-        </MuiGrid>
-      </Box>
-    )
+            </Link>
+          </MuiGrid>
+        ))}
+      </MuiGrid>
+    </Box>
+  );
 }
