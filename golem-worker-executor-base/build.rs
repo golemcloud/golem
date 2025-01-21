c7a6ff858af7f64b7a6ff75cbeb468fcbfe2b137
Afsal Thaj <afsal.taj06@gmail.com>
2025-01-21 23:39:35 +1100

    Add fork

--- golem-worker-executor-base/build.rs
+++ golem-worker-executor-base/build.rs
@@ -65,6 +65,7 @@ fn preview2_mod_gen(golem_wit_path: &str) -> String {
           world golem {{
               import golem:api/host@0.2.0;
               import golem:api/host@1.1.0;
+              import golem:api/host@1.2.0;
               import golem:api/oplog@1.1.0;
               import golem:api/durability@1.2.0;
 
