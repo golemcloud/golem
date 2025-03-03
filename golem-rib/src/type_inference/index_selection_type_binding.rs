64eaab6d690ddf7e3318b2e1ab589e567b093ccc
Afsal Thaj <afsal.taj06@gmail.com>
2025-03-03 12:31:25 +1100

    Bind default types

--- golem-rib/src/type_inference/index_selection_type_binding.rs
+++ golem-rib/src/type_inference/index_selection_type_binding.rs
@@ -1,8 +1,10 @@
 use crate::{Expr, InferredType};
 use std::collections::VecDeque;
+use std::ops::DerefMut;
 
 // All select indices with literal numbers don't need to explicit
-// type annotation, and will be automatically inferred as u64
+// type annotation to get better developer experience,
+// and all literal numbers will be automatically inferred as u64
 pub fn bind_default_types_to_index_expressions(expr: &mut Expr) {
     let mut queue = VecDeque::new();
     queue.push_back(expr);
@@ -12,11 +14,23 @@ pub fn bind_default_types_to_index_expressions(expr: &mut Expr) {
             Expr::SelectIndex { expr, index, .. } => {
                 let existing = index.inferred_type();
                 if existing.is_unknown() || existing.is_one_of() {
-                    if let Expr::Number { inferred_type, .. } = &mut **index {
+                    if let Expr::Number { inferred_type, .. } = index.deref_mut() {
                         *inferred_type = InferredType::U64
                     }
                 }
                 queue.push_back(expr);
+            }
+
+            Expr::Range { range, .. } => {
+                let exprs = range.get_exprs_mut();
+
+                for expr in exprs {
+                    if let Expr::Number { inferred_type, .. } = expr.deref_mut() {
+                        if inferred_type.is_unknown() || inferred_type.is_one_of() {
+                            *inferred_type = InferredType::U64
+                        }
+                    }
+                }
             }
 
             _ => expr.visit_children_mut_bottom_up(&mut queue),
