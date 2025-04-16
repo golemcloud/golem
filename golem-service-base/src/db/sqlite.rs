c0830ea02bafffee365227e51a078963c0c7dfc3
Dávid István Bíró <noise64@gmail.com>
2025-04-16 17:19:53 +0200

    Make service start functions "Send" (#1534)

--- golem-service-base/src/db/sqlite.rs
+++ golem-service-base/src/db/sqlite.rs
@@ -358,7 +358,8 @@ pub async fn migrate(
     info!("DB migration: sqlite://{}", config.database);
     let mut conn = SqliteConnection::connect_with(&config.connect_options()).await?;
     let migrator = sqlx::migrate::Migrator::new(migrations).await?;
-    migrator.run(&mut conn).await?;
+    // See: https://github.com/launchbadge/sqlx/issues/954, Send required for golem-cli
+    futures::executor::block_on(migrator.run(&mut conn))?;
     let _ = conn.close().await;
     Ok(())
 }
