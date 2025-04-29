458b7db244d5db57539db3d42a80b3a8a59b5f0c
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2025-04-29 22:10:26 +0100

    feat: added implementation for OpenRouter

---
+++ golem-llm/llm-openrouter/src/lib.rs
@@ -0,0 +1,60 @@
+wit_bindgen::generate!({
+    world: "libllm",
+    path: "../llm.wit",
+});
+
+mod api;
+mod types;
+
+use crate::exports::golem::llm::llm;
+
+struct Component;
+
+impl llm::Guest for Component {
+    type ChatStream = types::ChatStream;
+
+    fn send(
+        messages: Vec<llm::Message>,
+        config: llm::Config,
+    ) -> Result<llm::ChatEvent, llm::Error> {
+        let api_key = std::env::var("OPENROUTER_API_KEY").map_err(|_| llm::Error {
+            code: llm::ErrorCode::InvalidRequest,
+            message: "OPENROUTER_API_KEY is missing".to_owned(),
+            provider_error_json: None,
+        })?;
+        let open_router = api::OpenRouter::new(api_key);
+
+        open_router.generate_completions(messages, vec![], config)
+    }
+
+    fn continue_inference(
+        messages: Vec<llm::Message>,
+        tool_results: Vec<llm::ToolResult>,
+        config: llm::Config,
+    ) -> Result<llm::ChatEvent, llm::Error> {
+        let api_key = std::env::var("OPENROUTER_API_KEY").map_err(|_| llm::Error {
+            code: llm::ErrorCode::InvalidRequest,
+            message: "OPENROUTER_API_KEY is missing".to_owned(),
+            provider_error_json: None,
+        })?;
+        let open_router = api::OpenRouter::new(api_key);
+
+        open_router.generate_completions(messages, tool_results, config)
+    }
+
+    fn stream_chat(
+        messages: Vec<llm::Message>,
+        config: llm::Config,
+    ) -> Result<llm::ChatStream, llm::Error> {
+        let api_key = std::env::var("OPENROUTER_API_KEY").map_err(|_| llm::Error {
+            code: llm::ErrorCode::InvalidRequest,
+            message: "OPENROUTER_API_KEY is missing".to_owned(),
+            provider_error_json: None,
+        })?;
+        let open_router = api::OpenRouter::new(api_key);
+
+        open_router.stream_completions(messages, config).map(llm::ChatStream::new)
+    }
+}
+
+export!(Component);
