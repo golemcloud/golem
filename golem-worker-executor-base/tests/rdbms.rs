dd44fe30666af6b8a9a1a408ce9d1726cf621563
Peter Kotula <peto.kotula@yahoo.com>
2025-05-15 00:07:03 +0200

    check_transaction_oplog_entries

--- golem-worker-executor-base/tests/rdbms.rs
+++ golem-worker-executor-base/tests/rdbms.rs
@@ -22,6 +22,10 @@ use crate::common::{start, TestContext, TestWorkerExecutor};
 use crate::{LastUniqueId, Tracing, WorkerExecutorTestDependencies};
 use assert2::check;
 use golem_api_grpc::proto::golem::worker::v1::worker_error::Error;
+use golem_common::model::public_oplog::{
+    EndRegionParameters, ImportedFunctionInvokedParameters, PublicDurableFunctionType,
+    PublicOplogEntry, TimestampParameter, WriteRemoteBatchedParameters,
+};
 use golem_common::model::{ComponentId, IdempotencyKey, OplogIndex, WorkerId, WorkerStatus};
 use golem_test_framework::components::rdb::docker_mysql::DockerMysqlRdb;
 use golem_test_framework::components::rdb::docker_postgres::DockerPostgresRdb;
@@ -33,6 +37,7 @@ use golem_worker_executor_base::services::rdbms::postgres::PostgresType;
 use golem_worker_executor_base::services::rdbms::RdbmsType;
 use serde_json::json;
 use tokio::task::JoinSet;
+use try_match::try_match;
 use uuid::Uuid;
 
 inherit_test_dep!(WorkerExecutorTestDependencies);
@@ -434,8 +439,10 @@ async fn rdbms_postgres_idempotency(
     check!(result2 == result1);
 
     let oplog = executor.get_oplog(&worker_id, OplogIndex::INITIAL).await;
-    let oplog = serde_json::to_string(&oplog);
-    check!(oplog.is_ok());
+    let oplog_json = serde_json::to_string(&oplog);
+    check!(oplog_json.is_ok());
+
+    check_transaction_oplog_entries::<PostgresType>(oplog);
 
     drop(executor);
 }
@@ -817,8 +824,10 @@ async fn rdbms_mysql_idempotency(
     check!(result2 == result1);
 
     let oplog = executor.get_oplog(&worker_id, OplogIndex::INITIAL).await;
-    let oplog = serde_json::to_string(&oplog);
-    check!(oplog.is_ok());
+    let oplog_json = serde_json::to_string(&oplog);
+    check!(oplog_json.is_ok());
+
+    check_transaction_oplog_entries::<MysqlType>(oplog);
 
     drop(executor);
 }
@@ -1172,4 +1181,55 @@ fn query_ok_response(
 
 fn query_empty_ok_response() -> serde_json::Value {
     query_ok_response(vec![], vec![])
+}
+
+fn check_transaction_oplog_entries<T: RdbmsType>(entries: Vec<PublicOplogEntry>) {
+    let mut begin_entry: Option<(usize, TimestampParameter)> = None;
+    let mut end_entry: Option<(usize, EndRegionParameters)> = None;
+    let mut imported_function_invoked: Vec<(usize, ImportedFunctionInvokedParameters)> = vec![];
+
+    for (i, e) in entries.iter().enumerate() {
+        match begin_entry.clone() {
+            Some(_) => {
+                if end_entry.is_none() {
+                    if let Ok(v) =
+                        try_match!(e.clone(), PublicOplogEntry::ImportedFunctionInvoked(v))
+                    {
+                        imported_function_invoked.push((i, v));
+                    }
+
+                    end_entry = try_match!(e.clone(), PublicOplogEntry::EndRemoteWrite(v1))
+                        .ok()
+                        .map(|v| (i, v));
+                }
+            }
+            None => {
+                begin_entry = try_match!(e.clone(), PublicOplogEntry::BeginRemoteWrite(v))
+                    .ok()
+                    .map(|v| (i, v));
+            }
+        }
+    }
+
+    check!(begin_entry.is_some());
+    check!(end_entry.is_some());
+
+    let (begin_index, _) = begin_entry.unwrap();
+    let (end_index, end_entry) = end_entry.unwrap();
+
+    check!(!imported_function_invoked.is_empty());
+
+    let fn_prefix = format!("rdbms::{}::db-transaction::", T::default());
+
+    for (i, v) in imported_function_invoked {
+        check!(i > begin_index);
+        check!(i < end_index);
+        check!(v.function_name.starts_with(fn_prefix.as_str()));
+        check!(
+            v.wrapped_function_type
+                == PublicDurableFunctionType::WriteRemoteBatched(WriteRemoteBatchedParameters {
+                    index: Some(end_entry.begin_index)
+                })
+        );
+    }
 }
