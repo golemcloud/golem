30a470bf7ce27dcb740493a7918556014a09b676
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2025-06-01 23:27:49 +0200

    wip

--- golem-test-framework/src/components/cloud_service/provided.rs
+++ golem-test-framework/src/components/cloud_service/provided.rs
@@ -12,84 +12,47 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::components::component_service::{
-    new_component_client, new_plugin_client, ComponentService, ComponentServiceClient,
-    PluginServiceClient,
-};
+use crate::components::cloud_service::new_project_client;
 use crate::config::GolemClientProtocol;
 use async_trait::async_trait;
 use golem_service_base::service::plugin_wasm_files::PluginWasmFilesService;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use tracing::info;
+use super::{CloudService, CloudServiceInternal, ProjectServiceClient};
 
-use super::ComponentServiceInternal;
-
-pub struct ProvidedComponentService {
-    component_directory: PathBuf,
+pub struct ProvidedCloudService {
     host: String,
     http_port: u16,
     grpc_port: u16,
-    component_client: ComponentServiceClient,
-    plugin_client: PluginServiceClient,
-    plugin_wasm_files_service: Arc<PluginWasmFilesService>,
+    project_client: ProjectServiceClient,
 }
 
-impl ProvidedComponentService {
+impl ProvidedCloudService {
     pub async fn new(
-        component_directory: PathBuf,
         host: String,
         http_port: u16,
         grpc_port: u16,
-        client_protocol: GolemClientProtocol,
-        plugin_wasm_files_service: Arc<PluginWasmFilesService>,
+        client_protocol: GolemClientProtocol
     ) -> Self {
-        info!("Using already running golem-component-service on {host}, http port: {http_port}, grpc port: {grpc_port}");
+        info!("Using already running cloud-service on {host}, http port: {http_port}, grpc port: {grpc_port}");
         Self {
-            component_directory,
             host: host.clone(),
             http_port,
             grpc_port,
-            component_client: new_component_client(client_protocol, &host, grpc_port, http_port)
-                .await,
-            plugin_client: new_plugin_client(client_protocol, &host, grpc_port, http_port).await,
-            plugin_wasm_files_service,
+            project_client: new_project_client(client_protocol, &host, grpc_port, http_port).await,
         }
     }
 }
 
 #[async_trait]
-impl ComponentServiceInternal for ProvidedComponentService {
-    fn component_client(&self) -> ComponentServiceClient {
-        self.component_client.clone()
-    }
-
-    fn plugin_client(&self) -> PluginServiceClient {
-        self.plugin_client.clone()
-    }
-
-    fn plugin_wasm_files_service(&self) -> Arc<PluginWasmFilesService> {
-        self.plugin_wasm_files_service.clone()
+impl CloudServiceInternal for ProvidedCloudService {
+    fn project_client(&self) -> ProjectServiceClient {
+        self.project_client.clone()
     }
 }
 
 #[async_trait]
-impl ComponentService for ProvidedComponentService {
-    fn component_directory(&self) -> &Path {
-        &self.component_directory
-    }
-
-    fn private_host(&self) -> String {
-        self.host.clone()
-    }
-
-    fn private_http_port(&self) -> u16 {
-        self.http_port
-    }
-
-    fn private_grpc_port(&self) -> u16 {
-        self.grpc_port
-    }
-
+impl CloudService for ProvidedCloudService {
     async fn kill(&self) {}
 }
