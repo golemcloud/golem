7276cb7854f4b5a7df5f66ccb56d60fb289d5fcf
Afsal Thaj <afsal.taj06@gmail.com>
2025-06-11 18:21:49 +1000

    ensure state

--- golem-rib/src/type_inference/stateful_instance.rs
+++ golem-rib/src/type_inference/stateful_instance.rs
@@ -12,19 +12,33 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::{CallType, Expr, ExprVisitor, InstanceCreationType};
+use crate::{CallType, Expr, ExprVisitor, ParsedFunctionReference};
 use uuid::Uuid;
 
+//
 pub fn ensure_stateful_instance(expr: &mut Expr) {
     let mut visitor = ExprVisitor::bottom_up(expr);
 
     while let Some(Expr::Call {
-        call_type: CallType::InstanceCreation(InstanceCreationType::Worker { worker_name, .. }),
+        call_type: CallType::Function { function_name, .. } ,
+        args,
         ..
     }) = visitor.pop_back()
     {
-        if worker_name.is_none() {
-            *worker_name = Some(Box::new(Expr::literal(Uuid::new_v4().to_string())));
+        dbg!("here??");
+        let function_name = function_name.to_parsed_function_name().function;
+
+        match function_name {
+            ParsedFunctionReference::Function { function } if function == "instance" => {
+                if args.is_empty() {
+                    let generated_worker = Uuid::new_v4().to_string();
+                    *args = vec![Expr::literal(
+                        generated_worker,
+                    )];
+                }
+            }
+
+            _ => {}
         }
     }
 }
