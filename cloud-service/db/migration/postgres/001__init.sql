092533bb38f53eed32027de2ea27814e17192974
Ajay RV <ajayrv6711@gmail.com>
2025-07-15 09:26:57 +0530

    Update project policy

--- cloud-service/db/migration/postgres/001__init.sql
+++ cloud-service/db/migration/postgres/001__init.sql
@@ -74,7 +74,8 @@ CREATE TABLE IF NOT EXISTS project_policies (
   delete_instance BOOLEAN NOT NULL,
   view_project_grants BOOLEAN NOT NULL,
   create_project_grants BOOLEAN NOT NULL,
-  delete_project_grants BOOLEAN NOT NULL
+  delete_project_grants BOOLEAN NOT NULL,
+  export_api_definition BOOLEAN NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tokens (
