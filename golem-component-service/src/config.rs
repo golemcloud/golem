7a4ed8c086f6c401a58cd5065164a725ab9f8f05
Vittorio Parrella <vittorioparrella+1@gmail.com>
2025-07-30 22:25:20 +0200

    fix: component service current dir test fix

--- golem-component-service/src/config.rs
+++ golem-component-service/src/config.rs
@@ -125,6 +125,8 @@ mod tests {
 
     #[test]
     pub fn compilation_can_be_disabled() {
+        env::set_current_dir( PathBuf::from(env!("CARGO_MANIFEST_DIR")) ).expect("Failed to set current directory");
+
         std::env::set_var("GOLEM__COMPILATION__TYPE", "Disabled");
         let cfg = make_config_loader().load().expect("Failed to load config");
         std::env::remove_var("GOLEM__COMPILATION__TYPE");
