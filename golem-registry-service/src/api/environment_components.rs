8ffe4ac2c0755a130870e414fe7605ec01a8b976
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2025-08-01 15:18:57 +0200

    component wip

--- golem-registry-service/src/api/environment_components.rs
+++ golem-registry-service/src/api/environment_components.rs
@@ -14,13 +14,15 @@
 
 use super::ApiResult;
 use super::model::CreateComponentRequest;
-use golem_common_next::model::component::Component;
+use golem_common_next::model::component::{Component, ComponentName};
 use golem_common_next::recorded_http_api_request;
 use golem_service_base_next::api_tags::ApiTags;
 use golem_service_base_next::model::auth::GolemSecurityScheme;
 use poem_openapi::payload::Json;
 use poem_openapi::*;
 use tracing::Instrument;
+use poem_openapi::param::Path;
+use golem_common_next::model::EnvironmentId;
 
 pub struct EnvironmentComponentsApi {}
 
@@ -30,15 +32,20 @@ impl EnvironmentComponentsApi {
     ///
     /// The request body is encoded as multipart/form-data containing metadata and the WASM binary.
     /// If the component type is not specified, it will be considered as a `Durable` component.
-    #[oai(path = "/", method = "post", operation_id = "create_component")]
+    #[oai(
+        path = "/",
+        method = "post",
+        operation_id = "create_component"
+    )]
     async fn create_component(
         &self,
+        environment_id: Path<EnvironmentId>,
         payload: CreateComponentRequest,
         token: GolemSecurityScheme,
     ) -> ApiResult<Json<Component>> {
         let record = recorded_http_api_request!("create_component",);
         let response = self
-            .create_component_internal(payload, token)
+            .create_component_internal(environment_id.0, payload, token)
             .instrument(record.span.clone())
             .await;
         record.result(response)
@@ -46,7 +53,37 @@ impl EnvironmentComponentsApi {
 
     async fn create_component_internal(
         &self,
+        _environment_id: EnvironmentId,
         _payload: CreateComponentRequest,
+        _token: GolemSecurityScheme,
+    ) -> ApiResult<Json<Component>> {
+        todo!()
+    }
+
+    /// Get a component in an environment by name
+    #[oai(
+        path = "/:component_name",
+        method = "get",
+        operation_id = "get_component_by_name"
+    )]
+    async fn get_component_by_name(
+        &self,
+        environment_id: Path<EnvironmentId>,
+        component_name: Path<ComponentName>,
+        token: GolemSecurityScheme,
+    ) -> ApiResult<Json<Component>> {
+        let record = recorded_http_api_request!("create_component",);
+        let response = self
+            .get_component_by_name_internal(environment_id.0, component_name.0, token)
+            .instrument(record.span.clone())
+            .await;
+        record.result(response)
+    }
+
+    async fn get_component_by_name_internal(
+        &self,
+        _environment_id: EnvironmentId,
+        _component_name: ComponentName,
         _token: GolemSecurityScheme,
     ) -> ApiResult<Json<Component>> {
         todo!()
