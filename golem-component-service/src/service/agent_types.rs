04ea2261d766641c243b8a7fe83ef5b07b49aa7f
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2025-08-20 11:45:54 -0400

    Remove the wrong cache attempt

--- golem-component-service/src/service/agent_types.rs
+++ golem-component-service/src/service/agent_types.rs
@@ -16,10 +16,7 @@ use crate::error::ComponentError;
 use crate::model::agent_types::RegisteredAgentType;
 use crate::service::component::ComponentService;
 use async_trait::async_trait;
-use dashmap::DashMap;
 use golem_common::model::component::ComponentOwner;
-use golem_common::model::ProjectId;
-use std::collections::BTreeMap;
 use std::fmt::Debug;
 use std::sync::Arc;
 
@@ -43,26 +40,19 @@ pub trait AgentTypesService: Debug + Send + Sync {
         }
         Ok(None)
     }
-
-    /// Notify the service that a deployment has been made for a given project,
-    /// so it can invalidate any related caches
-    async fn project_changed(&self, _project_id: &ProjectId) {
-        // Default implementation does nothing
-    }
 }
 
+// NOTE: we cannot cache on this level currently, because changes to the set of components
+// can happen on other omponent-service nodes too. This should be revisited after the
+// atomic deployment changes.
 #[derive(Debug)]
 pub struct AgentTypesServiceDefault {
     component_service: Arc<dyn ComponentService>,
-    cache: DashMap<ProjectId, BTreeMap<String, RegisteredAgentType>>,
 }
 
 impl AgentTypesServiceDefault {
     pub fn new(component_service: Arc<dyn ComponentService>) -> Self {
-        Self {
-            component_service,
-            cache: DashMap::new(),
-        }
+        Self { component_service }
     }
 }
 
@@ -72,51 +62,17 @@ impl AgentTypesService for AgentTypesServiceDefault {
         &self,
         owner: &ComponentOwner,
     ) -> Result<Vec<RegisteredAgentType>, ComponentError> {
-        if let Some(cached) = self.cache.get(&owner.project_id) {
-            Ok(cached.values().cloned().collect())
-        } else {
-            let components = self.component_service.find_by_name(None, owner).await?;
-            let mut agent_types = Vec::new();
-            for component in components {
-                agent_types.extend(component.metadata.agent_types().iter().cloned().map(
-                    |agent_type| RegisteredAgentType {
-                        agent_type,
-                        implemented_by: component.versioned_component_id.component_id.clone(),
-                    },
-                ));
-            }
-            agent_types.sort_by_key(|r| r.agent_type.type_name.clone());
-            self.cache.insert(
-                owner.project_id.clone(),
-                BTreeMap::from_iter(
-                    agent_types
-                        .iter()
-                        .map(|r| (r.agent_type.type_name.clone(), r.clone())),
-                ),
-            );
-            Ok(agent_types)
+        let components = self.component_service.find_by_name(None, owner).await?;
+        let mut agent_types = Vec::new();
+        for component in components {
+            agent_types.extend(component.metadata.agent_types().iter().cloned().map(
+                |agent_type| RegisteredAgentType {
+                    agent_type,
+                    implemented_by: component.versioned_component_id.component_id.clone(),
+                },
+            ));
         }
-    }
-
-    async fn get_agent_type(
-        &self,
-        agent_type: &str,
-        owner: &ComponentOwner,
-    ) -> Result<Option<RegisteredAgentType>, ComponentError> {
-        if let Some(cached) = self.cache.get(&owner.project_id) {
-            Ok(cached.get(agent_type).cloned())
-        } else {
-            let all = self.get_all_agent_types(owner).await?;
-            for registered_agent_type in all {
-                if registered_agent_type.agent_type.type_name == agent_type {
-                    return Ok(Some(registered_agent_type));
-                }
-            }
-            Ok(None)
-        }
-    }
-
-    async fn project_changed(&self, project_id: &ProjectId) {
-        self.cache.remove(project_id);
+        agent_types.sort_by_key(|r| r.agent_type.type_name.clone());
+        Ok(agent_types)
     }
 }
