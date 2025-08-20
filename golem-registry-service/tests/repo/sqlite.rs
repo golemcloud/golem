883e043ce1cfd9657cd67e043262c351ab1d8d5e
Dávid István Bíró <noise64@gmail.com>
2025-08-20 10:01:54 +0200

    use sqlite migrations in repo tests

--- golem-registry-service/tests/repo/sqlite.rs
+++ golem-registry-service/tests/repo/sqlite.rs
@@ -53,8 +53,7 @@ impl SqliteDb {
 
         db::sqlite::migrate(
             &db_config,
-            // NOTE: for now we are using the POSTGRES migrations (until they become incompatible)
-            MigrationsDir::new("db/migration".into()).postgres_migrations(),
+            MigrationsDir::new("db/migration".into()).sqlite_migrations(),
         )
         .await
         .unwrap();
