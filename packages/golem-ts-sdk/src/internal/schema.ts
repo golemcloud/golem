5d895a08b6e436986b0af32e4b953d74e98aa6c7
Afsal Thaj <afsal.taj06@gmail.com>
2025-09-07 17:14:37 +0530

    add full stop

--- packages/golem-ts-sdk/src/internal/schema.ts
+++ packages/golem-ts-sdk/src/internal/schema.ts
@@ -126,7 +126,7 @@ export function buildInputSchema(
           return [parameterInfo[0], result] as [string, ElementSchema];
         },
         (err) =>
-          `${err}, found in method \`${methodName}\`, parameter \`${parameterInfo[0]}\`. Please replace this parameter type with a simpler, supported type"`,
+          `${err}, found in method \`${methodName}\`, parameter \`${parameterInfo[0]}\`. Please replace this parameter type with a simpler, supported type."`,
       ),
     ),
   );
