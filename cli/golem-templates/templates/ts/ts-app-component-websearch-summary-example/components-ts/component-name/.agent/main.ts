8c8229617a9353808a0e3b23251a31f43a70716a
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2025-09-08 18:27:25 +0200

    add websearch agent template

---
+++ cli/golem-templates/templates/ts/ts-app-component-websearch-summary-example/components-ts/component-name/.agent/main.ts
@@ -0,0 +1,11 @@
+import { TypescriptTypeRegistry } from '@golemcloud/golem-ts-sdk';
+import { Metadata } from '../.metadata/generated-types';
+
+TypescriptTypeRegistry.register(Metadata);
+
+// Import the user module after metadata is ready
+// This needs to be done this way otherwise rollup ends up generating the module,
+// where loading the metadata comes after the user module is loaded - resulting in errors.
+export default (async () => {
+    return await import("../src/main");
+})();
