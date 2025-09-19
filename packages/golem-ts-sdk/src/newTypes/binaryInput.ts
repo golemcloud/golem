678939d12fa2adb9b9643a0b5104d014b0bbf2c1
Afsal Thaj <afsal.taj06@gmail.com>
2025-09-19 15:34:17 +0530

    unstructured binary

---
+++ packages/golem-ts-sdk/src/newTypes/binaryInput.ts
@@ -0,0 +1,93 @@
+// Copyright 2024-2025 Golem Cloud
+//
+// Licensed under the Golem Source License v1.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://license.golem.cloud/LICENSE
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { ElementSchema, BinaryReference } from 'golem:agent/common';
+
+
+/**
+ * Represents unstructured binary input, which can be either a URL or inline binary data.
+ *
+ * Example usage:
+ *
+ * ```ts
+ * const urlBinary: UnstructuredBinary = UnstructuredBinary.fromUrl("https://example.com/file.bin");
+ * const inlineBinary: UnstructuredBinary = UnstructuredBinary.fromInline(new
+ *   Uint8Array([0x00, 0x01, 0x02]), "application/octet-stream"
+ * );
+ * ```
+ */
+export type UnstructuredBinary =
+  | {
+  tag: 'url';
+  val: string;
+}
+  | {
+  tag: 'inline';
+  val: Uint8Array;
+  mimeType?: string;
+};
+
+
+export const BinarySchema = {
+  fromMimeType(mimeTypes?: string[]): ElementSchema {
+    if (mimeTypes) {
+      return {
+        tag: 'unstructured-binary',
+        val: {
+          restrictions: mimeTypes.map((mimeType) => { return {mimeType: mimeType} })
+        }
+      }
+    }
+
+    return {
+      tag: 'unstructured-binary',
+      val: {}
+    }
+
+  }
+}
+
+export const UnstructuredText = {
+  fromDataValue(dataValue: BinaryReference): UnstructuredBinary {
+    if (dataValue.tag === 'url') {
+      return {
+        tag: 'url',
+        val: dataValue.val,
+      };
+    }
+
+    return {
+      tag: 'inline',
+      val: dataValue.val.data,
+      mimeType: dataValue.val.binaryType?.mimeType
+    };
+  },
+
+
+  fromUrl(urlValue: string): UnstructuredBinary {
+    return {
+      tag: 'url',
+      val: urlValue,
+    };
+  },
+
+
+  fromInline(data: Uint8Array, mimeType?: string): UnstructuredBinary {
+    return {
+      tag: 'inline',
+      val: data,
+      mimeType: mimeType,
+    };
+  },
+};
