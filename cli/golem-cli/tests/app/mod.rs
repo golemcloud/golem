2496f39ad6abdc037dac07ca835de68e54d581d1
Dávid István Bíró <noise64@gmail.com>
2025-09-30 12:04:43 +0200

    cleanup and extend CLI test for complex agent names

--- cli/golem-cli/tests/app/mod.rs
+++ cli/golem-cli/tests/app/mod.rs
@@ -52,6 +52,7 @@ mod cmd {
     pub static NEW: &str = "new";
     pub static PLUGIN: &str = "plugin";
     pub static REGISTER: &str = "register";
+    pub static REPL: &str = "repl";
     pub static TEMPLATES: &str = "templates";
 }
 
@@ -59,6 +60,8 @@ mod flag {
     pub static DEV_MODE: &str = "--dev-mode";
     pub static FORCE_BUILD: &str = "--force-build";
     pub static REDEPLOY_ALL: &str = "--redeploy-all";
+    pub static SCRIPT: &str = "--script";
+    pub static FORMAT: &str = "--format";
     pub static SHOW_SENSITIVE: &str = "--show-sensitive";
     pub static YES: &str = "--yes";
 }
@@ -1498,7 +1501,7 @@ async fn test_http_api_merging() {
 }
 
 #[test]
-async fn test_invoke_wasm_casing() {
+async fn test_invoke_and_repl_agent_id_casing_and_normalizing() {
     let mut ctx = TestContext::new();
     let app_name = "common_dep_plug_errors";
 
@@ -1532,20 +1535,19 @@ async fn test_invoke_wasm_casing() {
 
             @agent()
             class LongAgentName extends BaseAgent {
+              params: Complex;
               constructor(params: Complex) {
                 super();
+                this.params = params;
               }
 
-              async ask(question: Complex): Promise<Complex> {
-                return {
-                  oneField: "1212",
-                  anotherField: 100
-                };
+              async ask(question: Complex): Promise<[Complex, Complex]> {
+                return [this.params, question];
               }
             }
         "# },
     )
-    .unwrap(); 
+    .unwrap();
 
     ctx.start_server();
 
@@ -1557,10 +1559,31 @@ async fn test_invoke_wasm_casing() {
             flag::REDEPLOY_ALL,
             r#"long-agent-name({one-field: "1212", another-field: 100})"#,
             "ask",
-            r#"{one-field: "1212", another-field: 100}"#,
+            r#"{one-field: "1", another-field: 2}"#,
         ])
         .await;
     assert!(outputs.success());
+    assert!(outputs.stdout_contains_ordered([
+        r#"long-agent-name({one-field:"1212",another-field:100})"#,
+        r#"({one-field: "1212", another-field: 100}, {one-field: "1", another-field: 2})"#,
+    ]));
+
+    let outputs = ctx
+        .cli([
+            cmd::REPL,
+            flag::FORMAT,
+            "json",
+            flag::SCRIPT,
+            r#"
+                let x = long-agent-name({one-field: "1212", another-field: 100});
+                x.ask({one-field: "1", another-field: 2}) 
+            "#,
+        ])
+        .await;
+    assert!(outputs.success());
+    assert!(outputs.stdout_contains(
+        r#"({"typ":{"type":"Tuple","items":[{"type":"Record","fields":[{"name":"one-field","typ":{"type":"Str"}},{"name":"another-field","typ":{"type":"F64"}}]},{"type":"Record","fields":[{"name":"one-field","typ":{"type":"Str"}},{"name":"another-field","typ":{"type":"F64"}}]}]},"value":[{"another-field":100.0,"one-field":"1212"},{"another-field":2.0,"one-field":"1"}]}"#
+    ));
 }
 
 enum CommandOutput {
