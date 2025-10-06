f0c11cbdea42a41e9bf46d4380571619d6147026
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2025-10-06 18:02:14 +0200

    Fixes

--- golem-rib/src/compiler/mod.rs
+++ golem-rib/src/compiler/mod.rs
@@ -1120,6 +1120,7 @@ mod compiler_error_tests {
                 component_dependency_key: ComponentDependencyKey {
                     component_name: "some_name".to_string(),
                     component_id: Uuid::new_v4(),
+                    component_version: 0,
                     root_package_name: None,
                     root_package_version: None,
                 },
