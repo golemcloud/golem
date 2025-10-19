a784fd2c56b1e2c68500cf92b5c2f7fb0366d98e
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2025-10-19 13:49:41 +0200

    CLI test groups and experiment with archiving all bins

--- cli/golem-cli/tests/lib.rs
+++ cli/golem-cli/tests/lib.rs
@@ -13,14 +13,14 @@
 // limitations under the License.
 
 use golem_common::tracing::{init_tracing_with_default_debug_env_filter, TracingConfig};
-use test_r::{tag_suite, test_dep};
+use test_r::{sequential_suite, tag_suite, test_dep};
 
 test_r::enable!();
 
 mod app;
 mod stubgen;
 
-tag_suite!(app, app);
+tag_suite!(stubgen, group1);
 
 #[derive(Debug)]
 pub struct Tracing;
