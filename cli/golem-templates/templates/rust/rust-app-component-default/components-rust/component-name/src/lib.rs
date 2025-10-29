1ca6a916428014a4d5f8300471ccb6a479a42b74
Afsal Thaj <afsal.taj06@gmail.com>
2025-10-29 11:19:17 +1100

    use schema in counter agent

--- cli/golem-templates/templates/rust/rust-app-component-default/components-rust/component-name/src/lib.rs
+++ cli/golem-templates/templates/rust/rust-app-component-default/components-rust/component-name/src/lib.rs
@@ -1,25 +1,20 @@
 use golem_rust::{Schema, agent_definition, agent_implementation};
 
 #[agent_definition]
-trait CounterAgent {
+trait Counter {
     fn new(init: CounterId) -> Self;
     fn increment(&mut self) -> i32;
 }
 
-#[derive(Schema)]
-struct CounterId {
-    id: String,
-}
-
-struct CounterAgentImpl {
+struct CounterImpl {
     init: i32,
     _id: CounterId,
 }
 
 #[agent_implementation]
-impl CounterAgent for CounterAgentImpl {
+impl Counter for CounterImpl {
     fn new(id: CounterId) -> Self {
-        CounterAgentImpl { _id: id, init: 0 }
+        CounterImpl { _id: id, init: 0 }
     }
     fn increment(&mut self) -> i32 {
         self.init += 1;
@@ -27,3 +22,7 @@ impl CounterAgent for CounterAgentImpl {
     }
 }
 
+#[derive(Schema)]
+struct CounterId {
+    id: String,
+}
