710e49c2f3a32ffa8edcb22db671dd93384eb0c1
Afsal Thaj <afsal.taj06@gmail.com>
2025-11-10 12:50:57 +1100

    reformat code

--- sdks/rust/golem-rust/src/agentic/async_utils.rs
+++ sdks/rust/golem-rust/src/agentic/async_utils.rs
@@ -2,14 +2,14 @@ use crate::agentic::get_reactor;
 use crate::wasm_rpc::golem_rpc_0_2_x::types::Pollable;
 use golem_wasm::{FutureInvokeResult, RpcError, WitValue};
 
-pub async fn await_invoke_result(
-    invoke_result: FutureInvokeResult,
-) -> Result<WitValue, RpcError> {
+pub async fn await_invoke_result(invoke_result: FutureInvokeResult) -> Result<WitValue, RpcError> {
     let golem_wasm_pollable = invoke_result.subscribe();
 
     await_pollable(golem_wasm_pollable).await;
 
-    invoke_result.get().expect("RPC invoke completed, but no result available")
+    invoke_result
+        .get()
+        .expect("RPC invoke completed, but no result available")
 }
 
 pub async fn await_pollable(pollable: Pollable) {
