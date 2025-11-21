90700ecc5e6e83be5eb021da6432b515d0bf127d
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2025-11-21 18:53:01 +0100

    Test proving that string encoding through rib + agents work (#2348)
    * Test proving that string encoding through rib + agents work
    
    * Fix

--- integration-tests/tests/api/api_deployment.rs
+++ integration-tests/tests/api/api_deployment.rs
@@ -1099,3 +1099,107 @@ async fn create_and_invoke_api_deployment_with_agent(
 
     Ok(())
 }
+
+#[test]
+#[tracing::instrument]
+async fn rib_api_mapping_correctly_encodes_strings(
+    deps: &EnvBasedTestDependencies,
+) -> anyhow::Result<()> {
+    let admin = deps.admin().await;
+    let project_id = admin.create_project().await;
+
+    let (_, component_name) = admin
+        .component("golem_it_constructor_parameter_echo")
+        .name("golem-it:constructor-parameter-echo")
+        .with_project(project_id.clone())
+        .store_and_get_name()
+        .await;
+
+    let api_definition = deps
+        .worker_service()
+        .create_api_definition(
+            &admin.token,
+            &project_id,
+            &HttpApiDefinitionRequest {
+                id: Uuid::new_v4().to_string(),
+                version: "1".to_string(),
+                draft: false,
+                security: None,
+                routes: vec![RouteRequestData {
+                    method: MethodPattern::Post,
+                    path: "/rib_api_mapping_correctly_encodes_strings/{agent-name}".to_string(),
+                    binding: GatewayBindingData {
+                        component: Some(GatewayBindingComponent {
+                            name: component_name.to_string(),
+                            version: Some(0),
+                        }),
+                        worker_name: None,
+                        response: Some(
+                            r#"
+                                let agent = echo-agent(request.path.agent-name);
+                                let input: string = request.body.input;
+                                let output = agent.return-input(input);
+                                let status: u64 = 200;
+                                {
+                                    headers: { ContentType: "json" },
+                                    body: { output: output },
+                                    status: 200
+                                }
+                            "#
+                            .to_string(),
+                        ),
+                        idempotency_key: None,
+                        binding_type: Some(GatewayBindingType::Default),
+                        invocation_context: None,
+                    },
+                    security: None,
+                }],
+            },
+        )
+        .await?;
+
+    let custom_request_port = deps.worker_service().public_custom_request_port();
+
+    // deploy api
+    {
+        let request = ApiDeploymentRequest {
+            project_id: project_id.0,
+            api_definitions: vec![ApiDefinitionInfo {
+                id: api_definition.id.clone(),
+                version: api_definition.version.clone(),
+            }],
+            site: ApiSite {
+                host: format!("127.0.0.1:{custom_request_port}"),
+                subdomain: None,
+            },
+        };
+
+        let _response = deps
+            .worker_service()
+            .create_or_update_api_deployment(&admin.token, request.clone())
+            .await?;
+    }
+
+    // call custom request api
+    {
+        let agent_name = Uuid::new_v4();
+        let input = "Hello \"World\"!\nThis is a string with newline,\r\nnand quotes.";
+
+        let client = reqwest::Client::new();
+
+        let response = client
+            .post(format!(
+                "http://127.0.0.1:{custom_request_port}/rib_api_mapping_correctly_encodes_strings/{agent_name}"
+            ))
+            .json(&json!({ "input": input }))
+            .send()
+            .await?;
+
+        assert!(response.status() == StatusCode::OK);
+
+        let response_body = response.json::<serde_json::Value>().await?;
+        assert!(response_body == json!({ "output": input }));
+    }
+
+    Ok(())
+}
