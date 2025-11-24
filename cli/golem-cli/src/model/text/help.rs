160d5ce0b8bbeae1b050492b20e3beccdf65982c
Dávid István Bíró <noise64@gmail.com>
2025-11-24 11:56:08 +0100

    make fix

--- cli/golem-cli/src/model/text/help.rs
+++ cli/golem-cli/src/model/text/help.rs
@@ -13,21 +13,19 @@
 // limitations under the License.
 
 use crate::log::{logln, LogColorize};
-use crate::model::component::{
-    agent_interface_name, render_type, show_exported_functions,
-};
+use crate::model::component::{agent_interface_name, render_type, show_exported_functions};
 use crate::model::text::fmt::{
     format_export, log_table, FieldsBuilder, MessageWithFields, MessageWithFieldsIndentMode,
     TextView,
 };
 use cli_table::Table;
 use colored::Colorize;
+use golem_client::model::ComponentDto;
 use golem_common::model::agent::AgentId;
 use golem_common::model::component::ComponentName;
 use golem_wasm::analysis::AnalysedType;
 use indoc::indoc;
 use textwrap::WordSplitter;
-use golem_client::model::ComponentDto;
 
 pub struct WorkerNameHelp;
 
