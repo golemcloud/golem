82c9797e47b8f5a08d13f4ed2c7389efd6ebb295
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2025-11-28 20:08:05 +0800

    wip

--- golem-client/build.rs
+++ golem-client/build.rs
@@ -226,9 +226,15 @@ fn generate(yaml_path: PathBuf, out_dir: OsString) {
             ),
             // Public Oplog
             (
+                "PublicOplogEntry",
+                "golem_common::model::oplog::PublicOplogEntry",
+            ),
+            (
                 "PublicOplogEntryWithIndex",
                 "golem_common::model::oplog::PublicOplogEntryWithIndex",
             ),
+            ("OplogCursor", "golem_common::model::oplog::OplogCursor"),
+            ("OplogRegion", "golem_common::model::regions::OplogRegion"),
             // Http api definition
             (
                 "HttpApiDefinitionCreation",
@@ -255,102 +261,22 @@ fn generate(yaml_path: PathBuf, out_dir: OsString) {
                 "HttpApiDeployment",
                 "golem_common::model::http_api_deployment::HttpApiDeployment",
             ),
-            // TODO: Leftovers
-            ("PluginScope", "golem_common::model::plugin::PluginScope"),
-            (
-                "RegisteredAgentType",
-                "golem_common::model::agent::RegisteredAgentType",
-            ),
-            ("ShardId", "golem_common::model::ShardId"),
-            ("ValueAndType", "golem_wasm_rpc::ValueAndType"),
-            ("AgentType", "golem_common::model::agent::AgentType"),
-            ("DataSchema", "golem_common::model::agent::DataSchema"),
-            ("AgentInstanceKey", "golem_common::model::AgentInstanceKey"),
-            (
-                "AgentInstanceDescription",
-                "golem_common::model::AgentInstanceDescription",
-            ),
-            ("AnalysedExport", "golem_wasm::analysis::AnalysedExport"),
-            ("AnalysedType", "golem_wasm::analysis::AnalysedType"),
-            ("PluginScope", "golem_common::model::plugin::PluginScope"),
-            (
-                "ComponentMetadata",
-                "golem_common::model::component_metadata::ComponentMetadata",
-            ),
-            (
-                "ComponentFilePathWithPermissionsList",
-                "golem_common::model::ComponentFilePathWithPermissionsList",
-            ),
-            ("ComponentType", "golem_common::model::ComponentType"),
-            ("DataValue", "golem_common::model::agent::DataValue"),
-            ("Empty", "golem_common::model::Empty"),
-            (
-                "InitialComponentFile",
-                "golem_common::model::InitialComponentFile",
-            ),
-            ("ErrorBody", "golem_common::model::error::ErrorBody"),
-            ("ErrorsBody", "golem_common::model::error::ErrorsBody"),
-            (
-                "ExportedResourceInstanceKey",
-                "golem_common::model::ExportedResourceInstanceKey",
-            ),
-            (
-                "ExportedResourceInstanceDescription",
-                "golem_common::model::ExportedResourceInstanceDescription",
-            ),
-            ("GolemError", "golem_common::model::error::GolemError"),
-            (
-                "PluginInstallationAction",
-                "golem_common::model::plugin::PluginInstallationAction",
-            ),
-            ("OplogCursor", "golem_common::model::oplog::OplogCursor"),
-            ("OplogRegion", "golem_common::model::regions::OplogRegion"),
-            (
-                "ProjectActions",
-                "golem_common::model::auth::ProjectActions",
-            ),
-            (
-                "ProjectPermission",
-                "golem_common::model::auth::ProjectPermission",
-            ),
-            ("PromiseId", "golem_common::model::PromiseId"),
-            (
-                "PublicOplogEntry",
-                "golem_common::model::oplog::PublicOplogEntry",
-            ),
-            (
-                "RegisteredAgentType",
-                "golem_common::model::agent::RegisteredAgentType",
-            ),
-            ("ShardId", "golem_common::model::ShardId"),
-            ("ValueAndType", "golem_wasm::ValueAndType"),
-            (
-                "ValueAndOptionalType",
-                "golem_wasm::json::OptionallyValueAndTypeJson",
-            ),
-            (
-                "WasiConfigVarsEntry",
-                "golem_common::model::worker::WasiConfigVarsEntry",
-            ),
-            (
-                "WasmRpcTarget",
-                "golem_common::model::component_metadata::WasmRpcTarget",
-            ),
-            (
-                "WorkerCreationRequest",
-                "golem_common::model::worker::WorkerCreationRequest",
-            ),
-            ("WorkerFilter", "golem_common::model::WorkerFilter"),
-            ("WorkerId", "golem_common::model::WorkerId"),
-            (
-                "WorkerBindingType",
-                "golem_common::model::WorkerBindingType",
-            ),
+            // worker
             (
                 "WorkerResourceDescription",
                 "golem_common::model::WorkerResourceDescription",
             ),
             ("WorkerStatus", "golem_common::model::WorkerStatus"),
+            // common
+            ("Empty", "golem_common::model::Empty"),
+            // golem wasm
+            ("ValueAndType", "golem_wasm::ValueAndType"),
+            (
+                "ValueAndOptionalType",
+                "golem_wasm::json::OptionallyValueAndTypeJson",
+            ),
+            ("AnalysedExport", "golem_wasm::analysis::AnalysedExport"),
+            ("AnalysedType", "golem_wasm::analysis::AnalysedType"),
         ],
         &["/v1/components/{component_id}/workers/{worker_name}/connect"],
     )
