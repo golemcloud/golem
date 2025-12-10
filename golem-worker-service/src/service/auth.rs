c68f1c18450903bdb16f7000d514766291a85725
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2025-12-10 11:45:18 -0500

    Fix benchmarks/worker-executor deadlock (#2477)
    * fix benchmarks/worker-executor deadlock
    
    * remove logs

--- golem-worker-service/src/service/auth.rs
+++ golem-worker-service/src/service/auth.rs
@@ -121,22 +121,28 @@ impl RemoteAuthService {
         environment_id: &EnvironmentId,
         auth_ctx: &AuthCtx,
     ) -> Result<Option<AuthDetailsForEnvironment>, AuthServiceError> {
+        // environment level auth does not care about impersonation, so downgrade here to avoid cache
+        // misses during rpc
+        let impersonated_auth = auth_ctx.impersonated();
         let result = self
             .environment_auth_details_cache
-            .get_or_insert_simple(&(*environment_id, auth_ctx.clone()), async move || {
-                self.client
-                    .get_auth_details_for_environment(environment_id, false, auth_ctx)
-                    .await
-                    .map_err(|e| match e {
-                        RegistryServiceError::NotFound(_) => {
-                            EnvironmentAuthDetailsCacheError::NotFound
-                        }
-                        e => {
-                            tracing::warn!("Authenticating user token failed: {e}");
-                            EnvironmentAuthDetailsCacheError::Error
-                        }
-                    })
-            })
+            .get_or_insert_simple(
+                &(*environment_id, impersonated_auth.clone()),
+                async move || {
+                    self.client
+                        .get_auth_details_for_environment(environment_id, false, &impersonated_auth)
+                        .await
+                        .map_err(|e| match e {
+                            RegistryServiceError::NotFound(_) => {
+                                EnvironmentAuthDetailsCacheError::NotFound
+                            }
+                            e => {
+                                tracing::warn!("Authenticating user token failed: {e}");
+                                EnvironmentAuthDetailsCacheError::Error
+                            }
+                        })
+                },
+            )
             .await
             .map(Some)
             .or_else(|e| match e {
