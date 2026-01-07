af0cbee2b67a39a331adbe36f2626b40979ba3a7
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2026-01-07 18:22:01 +0100

    fmt

--- sdks/ts/packages/golem-ts-sdk/src/internal/schema.ts
+++ sdks/ts/packages/golem-ts-sdk/src/internal/schema.ts
@@ -246,7 +246,7 @@ export function getAgentMethodSchema(
         inputSchema: inputSchema,
         outputSchema: outputSchema,
         // TODO
-        httpEndpoint: []
+        httpEndpoint: [],
       });
     }),
   );
