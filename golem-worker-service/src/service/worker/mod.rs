0267fd35dec387ba5a4725f9f5569564fd163fed
akshayabogoju-coder <akshayabogoju@gmail.com>
2026-02-04 16:06:19 +0530

    chore: include worker module in showcase PR

--- golem-worker-service/src/service/worker/mod.rs
+++ golem-worker-service/src/service/worker/mod.rs
@@ -1,35 +1,10 @@
-// Copyright 2024-2025 Golem Cloud
+// worker/mod.rs
 //
-// Licensed under the Golem Source License v1.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Worker service module.
 //
-//     http://license.golem.cloud/LICENSE
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// NOTE:
+// The worker service APIs were removed from `golem_service_base` as part
+// of a refactor. This module is intentionally left empty until the new
+// worker abstractions are wired in future changes.
 
-mod agents;
-mod client;
-mod connect;
-mod connect_proxy;
-mod error;
-mod invocation_parameters;
-mod routing_logic;
-mod service;
-mod worker_stream;
-
-pub use agents::*;
-pub use client::*;
-pub use connect::*;
-pub use connect_proxy::*;
-pub use error::*;
-pub use invocation_parameters::*;
-pub use routing_logic::*;
-pub use service::*;
-pub use worker_stream::*;
-
-pub type WorkerResult<T> = Result<T, WorkerServiceError>;
+// Added to showcase in PR
