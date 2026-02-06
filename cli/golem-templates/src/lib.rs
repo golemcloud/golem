648d6fea55f4c576037c531d8e23d328b7c2ffbe
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2026-02-06 18:56:12 +0100

    update golem-ai

--- cli/golem-templates/src/lib.rs
+++ cli/golem-templates/src/lib.rs
@@ -38,9 +38,9 @@ macro_rules! app_manifest_version {
         "1.5.0-dev.1"
     };
 }
-static GOLEM_RUST_VERSION: &str = "1.11.0";
+static GOLEM_RUST_VERSION: &str = "1.12.0-dev.1";
 static GOLEM_TS_VERSION: &str = "0.1.0";
-static GOLEM_AI_VERSION: &str = "v0.4.0";
+static GOLEM_AI_VERSION: &str = "v0.5.0-dev.1";
 static GOLEM_AI_SUFFIX: &str = ".wasm";
 
 static TEMPLATES: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/templates");
