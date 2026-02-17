4270af6074a4300929ad25465a9b4749ebb931bd
Afsal Thaj <afsal.taj06@gmail.com>
2026-02-17 18:20:11 +1100

    revert changes within decorator

--- sdks/ts/packages/golem-ts-sdk/src/decorators/agent.ts
+++ sdks/ts/packages/golem-ts-sdk/src/decorators/agent.ts
@@ -34,7 +34,6 @@ import { getHttpMountDetails } from '../internal/http/mount';
 import { validateHttpMount } from '../internal/http/validation';
 import { getAgentConstructorSchema } from '../internal/schema/constructor';
 import { getAgentMethodSchema } from '../internal/schema/method';
-import { AgentIdRegistry } from '../internal/registry/agentIdRegistry';
 
 export type AgentDecoratorOptions = {
   name?: string;
@@ -301,13 +300,22 @@ export function agent(options?: AgentDecoratorOptions) {
           };
         }
 
-        const agentId = AgentIdRegistry.get();
+        const instance = new ctor(...deserializedConstructorArgs.val);
 
-        if (!agentId) {
-          throw new Error('Internal Error: Unavailable agent id');
+        const agentId = getRawSelfAgentId();
+
+        if (!agentId.value.startsWith(agentTypeName.asWit)) {
+          const error = createCustomError(
+            `Expected the container name in which the agent is initiated to start with "${agentTypeName.asWit}", got "${agentId.value}"`,
+          );
+
+          return {
+            tag: 'err',
+            val: error,
+          };
         }
 
-        const instance = new ctor(...deserializedConstructorArgs.val);
+        instance.getId = () => agentId;
 
         const resolvedAgent = new ResolvedAgent(
           instance,
