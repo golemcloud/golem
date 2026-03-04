84c634fd066218e4659c47f9ba89b7bbde5f875d
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2026-03-04 16:26:15 +0100

    Copyright and license update in headers

--- cli/golem-cli/src/app/build/clean.rs
+++ cli/golem-cli/src/app/build/clean.rs
@@ -5,9 +5,9 @@ use crate::model::app::CleanMode;
 use golem_common::model::component::ComponentName;
 use std::collections::BTreeSet;
 use std::path::PathBuf;
-// Copyright 2024-2025 Golem Cloud
+// Copyright 2024-2026 Golem Cloud
 //
-// Licensed under the Golem Source License v1.0 (the "License");
+// Licensed under the Golem Source License v1.1 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
