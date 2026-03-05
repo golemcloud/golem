f7339438d71f67ea93dbe4a5850b213c09f3840d
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2026-03-05 21:18:38 +0100

    agent-config: automatically create/update secrets as part of deployment

--- golem-test-framework/src/config/dsl_impl.rs
+++ golem-test-framework/src/config/dsl_impl.rs
@@ -966,6 +966,7 @@ impl<Deps: TestDependencies> TestDslExtended for TestUserContext<Deps> {
                     current_revision: plan.current_revision,
                     expected_deployment_hash: plan.deployment_hash,
                     version: DeploymentVersion(Uuid::new_v4().to_string()),
+                    agent_secret_defaults: Vec::new(),
                 },
             )
             .await?;
