7d1f1f5a29fcdb4ee44e72ed2a9f0be5dc3b4e2f
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2026-03-06 13:51:35 +0100

    Format

--- cli/golem-cli/src/command_handler/component/mod.rs
+++ cli/golem-cli/src/command_handler/component/mod.rs
@@ -77,43 +77,43 @@ impl ComponentCommandHandler {
         subcommand: ComponentSubcommand,
     ) -> std::pin::Pin<Box<dyn std::future::Future<Output = anyhow::Result<()>> + '_>> {
         Box::pin(async move {
-        match subcommand {
-            ComponentSubcommand::New {
-                component_template,
-                component_name,
-            } => self.cmd_new(component_template, component_name).await,
-            ComponentSubcommand::Templates { filter } => self.cmd_templates(filter),
-            ComponentSubcommand::List => self.cmd_list().await,
-            ComponentSubcommand::Get {
-                component_name,
-                revision,
-            } => self.cmd_get(component_name.component_name, revision).await,
+            match subcommand {
+                ComponentSubcommand::New {
+                    component_template,
+                    component_name,
+                } => self.cmd_new(component_template, component_name).await,
+                ComponentSubcommand::Templates { filter } => self.cmd_templates(filter),
+                ComponentSubcommand::List => self.cmd_list().await,
+                ComponentSubcommand::Get {
+                    component_name,
+                    revision,
+                } => self.cmd_get(component_name.component_name, revision).await,
 
-            ComponentSubcommand::UpdateAgents {
-                component_name,
-                update_mode,
-                r#await,
-                disable_wakeup,
-            } => {
-                self.cmd_update_workers(
-                    component_name.component_name,
+                ComponentSubcommand::UpdateAgents {
+                    component_name,
                     update_mode,
                     r#await,
                     disable_wakeup,
-                )
-                .await
-            }
-            ComponentSubcommand::RedeployAgents { component_name } => {
-                self.cmd_redeploy_workers(component_name.component_name)
+                } => {
+                    self.cmd_update_workers(
+                        component_name.component_name,
+                        update_mode,
+                        r#await,
+                        disable_wakeup,
+                    )
                     .await
+                }
+                ComponentSubcommand::RedeployAgents { component_name } => {
+                    self.cmd_redeploy_workers(component_name.component_name)
+                        .await
+                }
+                ComponentSubcommand::Diagnose { component_name } => {
+                    self.cmd_diagnose(component_name).await
+                }
+                ComponentSubcommand::ManifestTrace { component_name } => {
+                    self.cmd_manifest_trace(component_name).await
+                }
             }
-            ComponentSubcommand::Diagnose { component_name } => {
-                self.cmd_diagnose(component_name).await
-            }
-            ComponentSubcommand::ManifestTrace { component_name } => {
-                self.cmd_manifest_trace(component_name).await
-            }
-        }
         })
     }
 
