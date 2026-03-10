b5f3e534a24514090e3de62a37541ad4ac675bd3
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2026-03-10 07:57:01 +0100

    Updated version to 1.4.0, updated readme

--- golem_sdk_example1/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-(cd ../golem_sdk_tools && moon run cmd -- reexports ../golem_sdk ../golem_sdk_example1/golem_moonbit_examples)
-(cd ../golem_sdk_tools && moon run cmd -- agents ../golem_sdk_example1/golem_moonbit_examples)
-moon build --target wasm --release
-wasm-tools component embed wit _build/wasm/release/build/golem_moonbit_examples/golem_moonbit_examples.wasm --encoding utf16 --output _build/wasm/release/golem_moonbit_examples.embed.wasm
-wasm-tools component new _build/wasm/release/golem_moonbit_examples.embed.wasm --output _build/wasm/release/golem_moonbit_examples.agent.wasm
