488ef5e63ae17b99298395de19955bd7e3c0eaf5
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2026-03-13 21:57:56 +0100

    fix

--- cli/golem-cli/src/command.rs
+++ cli/golem-cli/src/command.rs
@@ -1321,7 +1321,6 @@ pub mod api {
 
     pub mod agent_secret {
         use crate::command::parse_agent_secret_path;
-        use crate::model::environment::EnvironmentReference;
         use clap::Subcommand;
         use golem_common::model::agent_secret::{
             AgentSecretId, AgentSecretPath, AgentSecretRevision,
@@ -1331,9 +1330,6 @@ pub mod api {
         pub enum AgentSecretSubcommand {
             /// Create Agent Secret in the environment
             Create {
-                /// The environment to create the secret in
-                #[arg(long)]
-                environment: Option<EnvironmentReference>,
                 /// Path of the secret to create. The casing of path segments will be normalized during creation.
                 #[arg(long, value_parser = parse_agent_secret_path)]
                 path: AgentSecretPath,
@@ -1369,11 +1365,7 @@ pub mod api {
             },
 
             /// List Agent Secrets
-            List {
-                /// The environment to list the secrets in
-                #[arg(long)]
-                environment: Option<EnvironmentReference>,
-            },
+            List,
         }
     }
 
