c77f5db00969c10900b0540415afadf8bc75a3e8
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2026-03-17 15:58:24 +0100

    Sequential test running

--- integration-tests/tests/lib.rs
+++ integration-tests/tests/lib.rs
@@ -35,6 +35,8 @@ tag_suite!(worker_local_agent_config, group2);
 tag_suite!(api, group2);
 tag_suite!(custom_api, group2);
 
+test_r::sequential_suite!(plugins);
+
 #[derive(Debug)]
 pub struct Tracing;
 
