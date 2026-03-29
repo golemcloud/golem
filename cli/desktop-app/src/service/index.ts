d4696355581a0fbc8bb2e5a86605f3f81b88f3c5
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2026-03-29 18:21:34 +0200

    Removed the old desktop app (#3085)

--- cli/desktop-app/src/service/index.ts
+++
@@ -1,9 +0,0 @@
-import { Service } from "@/service/client.ts";
-// @ts-nocheck
-// import { fetchCurrentIP } from "@/lib/tauri&web.ts";
-
-export let API: Service = new Service();
-
-(async () => {
-  API = new Service();
-})();
