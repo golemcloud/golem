8bd3d1ce200f743d49fdc392f8c49f67c0daead6
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2026-04-07 10:28:33 +0200

    Better variant handling in the TS SDK (#3126)
    * Better variant handling
    
    * Format

--- sdks/ts/packages/golem-ts-repl/src/language-service.ts
+++ sdks/ts/packages/golem-ts-repl/src/language-service.ts
@@ -963,7 +963,7 @@ function getArgumentTypePlaceholders(
     return buildTypePlaceholders(type, checker);
   }
 
-  const variant = tagged.variants.find((entry) => entry.tagValue === tagValue);
+  const variant = tagged.variants.find((entry) => entry.tagKey === tagValue);
   if (!variant) {
     return buildTypePlaceholders(type, checker);
   }
@@ -1180,38 +1180,44 @@ function buildTaggedObjectPlaceholder(
 function getTaggedUnionInfo(
   unionTypes: tsm.Type[],
   checker: tsm.TypeChecker,
-): { tagName: string; variants: Array<{ type: tsm.Type; tagValue: string }> } | undefined {
+):
+  | {
+      tagName: string;
+      variants: Array<{ type: tsm.Type; tagValue: string; tagKey: string }>;
+    }
+  | undefined {
   if (!unionTypes.length) return;
   if (!unionTypes.every((type) => isObjectType(type))) return;
 
-  const firstProps = unionTypes[0].getProperties();
-  for (const prop of firstProps) {
-    const name = prop.getName();
-    const variants: Array<{ type: tsm.Type; tagValue: string }> = [];
+  const variants: Array<{ type: tsm.Type; tagValue: string; tagKey: string }> = [];
 
-    for (const variantType of unionTypes) {
-      const variantProp = variantType.getProperty(name);
-      if (!variantProp) {
-        variants.length = 0;
-        break;
-      }
+  for (const variantType of unionTypes) {
+    const variantProps = variantType.getProperties();
+    const payloadProps = variantProps.filter((prop) => prop.getName() !== 'tag');
 
-      const propType = getSymbolType(variantProp, checker);
-      const literalValue = propType ? getLiteralPlaceholder(propType) : undefined;
-      if (!literalValue) {
-        variants.length = 0;
-        break;
-      }
-
-      variants.push({ type: variantType, tagValue: literalValue });
+    if (payloadProps.length > 1) {
+      return undefined;
     }
 
-    if (variants.length === unionTypes.length) {
-      return { tagName: name, variants };
+    const variantProp = variantType.getProperty('tag');
+    if (!variantProp) {
+      return undefined;
     }
+
+    const propType = getSymbolType(variantProp, checker);
+    const literalValue = propType ? getLiteralPlaceholder(propType) : undefined;
+    if (!literalValue) {
+      return undefined;
+    }
+
+    variants.push({
+      type: variantType,
+      tagValue: literalValue,
+      tagKey: normalizeLiteralValue(literalValue),
+    });
   }
 
-  return undefined;
+  return { tagName: 'tag', variants };
 }
 
 function getObjectLiteralTagValue(
@@ -1230,12 +1236,23 @@ function getObjectLiteralTagValue(
       case tsm.SyntaxKind.NumericLiteral:
       case tsm.SyntaxKind.TrueKeyword:
       case tsm.SyntaxKind.FalseKeyword:
-        return initializer.getText();
+        return normalizeLiteralValue(initializer.getText());
       default:
         return undefined;
     }
   }
   return undefined;
+}
+
+function normalizeLiteralValue(text: string): string {
+  if (text.length >= 2) {
+    const quote = text[0];
+    if ((quote === '"' || quote === "'" || quote === '`') && text[text.length - 1] === quote) {
+      return text.slice(1, -1);
+    }
+  }
+
+  return text;
 }
 
 function uniquePlaceholders(entries: string[]): string[] {
