73d2ba42682707162d691080a5c248717c765d74
Dávid István Bíró <noise64@gmail.com>
2026-04-14 19:05:20 +0200

    Bump SDK versions for rc1 (#3182)

--- cli/golem-cli/src/versions.rs
+++ cli/golem-cli/src/versions.rs
@@ -13,9 +13,9 @@
 // limitations under the License.
 
 pub mod sdk {
-    pub const RUST: &str = "2.0.0-dev.4";
-    pub const TS: &str = "1.0.0-dev.3";
-    pub const SCALA: &str = "1.5.0-M7";
+    pub const RUST: &str = "2.0.0-dev.7";
+    pub const TS: &str = "1.0.0-dev.5";
+    pub const SCALA: &str = "1.5.0-M8";
     pub const MANIFEST: &str = "1.5.0";
 
     #[macro_export]
