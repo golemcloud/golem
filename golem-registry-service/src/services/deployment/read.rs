ea4c53a21abc94b7c37deb08981c130ef6632566
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2026-06-08 21:42:29 +0200

    fmt

--- golem-registry-service/src/services/deployment/read.rs
+++ golem-registry-service/src/services/deployment/read.rs
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use super::mirror::{DeployedAgentTypeMirror, ResolvedAgentTypeMirror, schema_mirror_error};
 use super::authorize_environment_permission;
+use super::mirror::{DeployedAgentTypeMirror, ResolvedAgentTypeMirror, schema_mirror_error};
 use crate::repo::deployment::DeploymentRepo;
 use crate::repo::model::deployment::DeployRepoError;
 use crate::services::application::{ApplicationError, ApplicationService};
