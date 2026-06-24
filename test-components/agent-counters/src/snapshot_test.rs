d96c9516400d9f616a9e1f70fe04aa6647994d49
kmatasfp <33095685+kmatas@users.noreply.github.com>
2026-06-23 19:18:43 -0700

    Enable deterministic density snapshots

--- test-components/agent-counters/src/snapshot_test.rs
+++ test-components/agent-counters/src/snapshot_test.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::busy_loop;
 
-#[agent_definition(snapshotting = "enabled")]
+#[agent_definition(snapshotting = "every(50)")]
 trait SnapshotCounter {
     fn new(id: String) -> Self;
     fn increment(&mut self) -> u32;
