ebc84bff89d1f3f7b33fd0d44ad0663df69eecf5
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2026-07-09 16:15:32 +0200

    Clippy fixes for Rust 1.97

--- cli/golem-cli/src/app/context.rs
+++ cli/golem-cli/src/app/context.rs
@@ -620,7 +620,7 @@ impl ApplicationContext {
 
                     for (site, deployment) in http_api_deployments {
                         logln(format!("  {}", site.to_string().log_color_highlight(),));
-                        for (agent_name, _) in deployment.value.agents.iter() {
+                        for agent_name in deployment.value.agents.keys() {
                             logln(format!("    {}", agent_name.as_str().log_color_highlight(),));
                         }
                     }
@@ -649,7 +649,7 @@ impl ApplicationContext {
 
                 for (site, deployment) in mcp_deployments {
                     logln(format!("  {}", site.to_string().log_color_highlight(),));
-                    for (agent_name, _) in deployment.value.agents.iter() {
+                    for agent_name in deployment.value.agents.keys() {
                         logln(format!("    {}", agent_name.as_str().log_color_highlight(),));
                     }
                 }
